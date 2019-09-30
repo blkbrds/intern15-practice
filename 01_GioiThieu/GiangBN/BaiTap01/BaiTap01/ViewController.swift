4af2d5865390baf4da4050105e0c1781aed06419
Giang Bui N <giang.bui@asiantech.vn>
2019-09-30 16:35:49 +0700

    init project BaiTap01

---
+++ 01_GioiThieu/GiangBN/BaiTap01/BaiTap01/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BaiTap01
+//
+//  Created by PCI0002 on 9/30/19.
+//  Copyright © 2019 Asian Tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
