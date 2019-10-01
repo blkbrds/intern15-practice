641f3c7c197a440489d672a1fcd21d5b314fc378
Phuoc Nguyen <phuoc.eamp@gmail.com>
2019-10-01 15:11:20 +0700

    Sửa lỗi

--- 02_SwiftBasic/PhuocN/Baitap02/MyPlayground.playground/Contents.swift
+++ 02_SwiftBasic/PhuocN/Baitap02/MyPlayground.playground/Contents.swift
@@ -7,14 +7,16 @@ func hinhcau(bankinh: Double) -> (Double,Double) {
 
 // in ra 50 si fibonacci dau tien va tinh tong
 func soFibo() {
-  var a = Array(repeating: 1, count: 50)
+  var a = 1
+  var b = 1
+  var fibo = 0
   var s = 0
-  for i in 0..<50{
-    if i > 1 {
-      a[i] = a[i - 2] + a[i - 1]
-    }
-    s = s + a[i]
-    print(a[i])
+  for _ in 0..<50{
+    fibo = a + b
+    a = b
+    b = fibo
+    s = s + fibo
+    print(fibo)
   }
   print("Tong: \(s)")
 }
@@ -28,41 +30,32 @@ func sinn(x: Float, n: Int) -> Float{
   return n % 2 == 0 ? temp : -temp
 }
 
-var n = 0
-var sinX: Float = 0
-while abs(sinn(x: 0.5235, n: n)) > 0.0000001 {
-  sinX = sinX + sinn(x: 0.5235, n: n)
-  n = n + 1
+func sinx(x: Float) -> Float{
+  var n = 0
+  var sinX: Float = 0
+  while abs(sinn(x: x, n: n)) > 0.0000001 {
+    sinX = sinX + sinn(x: x, n: n)
+    n = n + 1
+  }
+  return sinX
 }
 
 //print(sinX)
 
- //diem so lan xuat hien cua chuoi con
-func tachchuoi(chuoiMe: String, chuoiCon: String) -> Int{
-  var a = chuoiMe
+//diem so lan xuat hien cua chuoi con
+func demSoChuoiCon(str: String, str1: String) -> Int{
   var count = 0
-  while chuoiCon.count < a.count {
-    count = a.hasPrefix(chuoiCon) ? count + 1 : count
-    a.removeFirst(1)
+  for i in 0...str.count - str1.count where str[str.index(str.startIndex, offsetBy: i)...str.index(str.startIndex, offsetBy: i + str1.count - 1)] == str1 {
+    count = count + 1
   }
+  
   return count
 }
-print(tachchuoi(chuoiMe: "abcaabbbabbbaac", chuoiCon: "ab"))
+print(demSoChuoiCon(str: "abcababaaababb", str1: "aba"))
 
 //random so thu tu trong mang
 func randomElement<T>(array: [T]) -> [T]{
-  var numberRandom = Int.random(in: 0..<array.count)
-  var previousRandom = [Int]()
-  var temp = array
-
-  array.forEach {
-    while previousRandom.contains(numberRandom) {
-      numberRandom = Int.random(in: 0..<array.count)
-    }
-    temp[numberRandom] = $0
-    previousRandom.append(numberRandom)
-  }
-  return temp
+  return array.shuffled()
 }
 
 var abc = [0,1,2,1,1,3,4,5,6,7,8,9]
@@ -71,8 +64,8 @@ print(randomElement(array: abc))
 // thay the 1 so trong mang
 func bearrayReplace<T: Comparable>(inputArray: [T], elemToReplace: T,substitutionElem: T) -> [T] {
   var temp = inputArray
-  for i in 0..<temp.count {
-    temp[i] = temp[i] == elemToReplace ? substitutionElem : temp[i]
+  for i in 0..<temp.count where temp[i] == elemToReplace{
+    temp[i] = substitutionElem
   }
   return temp
 }
