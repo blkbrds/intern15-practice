6f71a4a067549112549e345c2dcfb866e906c19d
Tai Nguyen D <tai.nguyen2@asiantech.vn>
2019-10-01 15:47:15 +0700

    no message

--- 01_GioiThieu/TaiN/BaiTap01/Nostoryboard/Nostoryboard/AppDelegate.swift
+++ 01_GioiThieu/TaiN/BaiTap01/Nostoryboard/Nostoryboard/AppDelegate.swift
@@ -10,25 +10,14 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.backgroundColor = .white
         let vc = MyViewController()
         window?.rootViewController = vc
         window?.makeKeyAndVisible()
-        
-        
-        
         return true
-        
-        
-        
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
