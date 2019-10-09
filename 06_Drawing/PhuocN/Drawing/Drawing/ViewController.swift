c3cfcc3fb4c5a3dd3de152b3b1ef93df5e396966
Phuoc Nguyen <phuoc.eamp@gmail.com>
2019-10-09 14:07:16 +0700

    Baitap 06: Drawing

--- 06_Drawing/PhuocN/Drawing/Drawing/ViewController.swift
+++ 06_Drawing/PhuocN/Drawing/Drawing/ViewController.swift
@@ -13,19 +13,21 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     self.view.backgroundColor = .secondarySystemGroupedBackground
-    //let view = LineChartView()
-    let view = PieChartView()
-    view.frame = CGRect(x: 0, y: 0, width: 400, height: 200)
-    view.center = self.view.center
-    view.backgroundColor = .clear
-    //    view.lines = [0,24,23,50,60]
-    //    view.thumbRadius = 10
-    //    view.thumbColor = .red
-    //    view.lineWidth = 5
-    view.pies = [Pie(value: 60, color: .blue, radius: 60),
-                 Pie(value: 70, color: .green, radius: 80),
-                 Pie(value: 20, color: .red, radius: 70),
-                 Pie(value: 120, color: .orange, radius: 55)]
-    self.view.addSubview(view)
+    let pieChartView = PieChartView()
+    pieChartView.frame = CGRect(x: 0, y: 100, width: 400, height: 200)
+    pieChartView.backgroundColor = .clear
+    pieChartView.pies = [Pie(value: 60, color: .blue, radius: 60),
+                    Pie(value: 70, color: .green, radius: 80),
+                    Pie(value: 20, color: .red, radius: 70),
+                    Pie(value: 120, color: .orange, radius: 55)]
+    self.view.addSubview(pieChartView)
+    let lineChartView = LineChartView()
+    lineChartView.frame = CGRect(x: 0, y: 400, width: 400, height: 300)
+    lineChartView.backgroundColor = .clear
+    lineChartView.lines = [0,24,23,50,60]
+    lineChartView.thumbRadius = 10
+    lineChartView.thumbColor = .red
+    lineChartView.lineWidth = 5
+    self.view.addSubview(lineChartView)
   }
 }
