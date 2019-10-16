3019719a52c3d268c1b36a585b0494b7fa8f41e1
Phuoc Nguyen <phuoc.eamp@gmail.com>
2019-10-16 15:08:42 +0700

    Baitap 10: Protocol bai 3 + 4

---
+++ 10_Protocol/PhuocN/Baitap3+4/PracticeTemplate/Protocol/Controllers/Ex3/Huyen/HuyenViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HuyenViewController.swift
+//  Protocol
+//
+//  Created by PhuocNguyen on 10/16/19.
+//  Copyright © 2019 Tien Le P. All rights reserved.
+//
+
+import UIKit
+
+class HuyenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ 10_Protocol/PhuocN/Baitap3+4/PracticeTemplate/Protocol/Models/Protocol/Location.swift
@@ -0,0 +1,13 @@
+//
+//  Location.swift
+//  Protocol
+//
+//  Created by PhuocNguyen on 10/16/19.
+//  Copyright © 2019 Tien Le P. All rights reserved.
+//
+
+import Foundation
+
+protocol Location {
+    
+}
