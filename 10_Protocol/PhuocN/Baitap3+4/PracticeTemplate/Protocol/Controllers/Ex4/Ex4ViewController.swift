14ddd509b1dc10577c24e6b770cb985bd7e9f4ab
Phuoc Nguyen <phuoc.eamp@gmail.com>
2019-10-17 09:53:59 +0700

    Bai tap 10: Protocol

--- 10_Protocol/PhuocN/Baitap3+4/PracticeTemplate/Protocol/Controllers/Ex4/Ex4ViewController.swift
+++ 10_Protocol/PhuocN/Baitap3+4/PracticeTemplate/Protocol/Controllers/Ex4/Ex4ViewController.swift
@@ -13,19 +13,11 @@ class Ex4ViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
-    
+ 
     // MARK: config
     override func setupUI() {
         super.setupUI()
         self.title = "Ex4"
     }
-    
-    override func setupData() {
-    }
-    
     
 }
