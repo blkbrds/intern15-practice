46baeed04aa2519e51cc78ec94292567a7dc08ee
Phuoc Nguyen <phuoc.eamp@gmail.com>
2019-10-23 10:17:35 +0700

    Bai tap 12: CollectionView

---
+++ 12_CollectionView/PhuocN/Baitap2/CollectionViewFlowLayout/CollectionViewFlowLayout/Controllers/PhotoStreamViewController 2.swift
@@ -0,0 +1,48 @@
+//
+//  PhotoStreamViewController.swift
+//  CollectionViewFlowLayout
+//
+//  Created by PhuocNguyen on 10/21/19.
+//  Copyright © 2019 PhuocNguyen. All rights reserved.
+//
+
+import UIKit
+
+final class PhotoStreamViewController: UIViewController {
+
+    private var images: [String] = ["iron", "Thor", "captain", "wolver", "doctor", "hulk" , "xprofessor", "hawkeye", "spider", "starlord", "deadpool", "jean", "storm", "magneto", "gamora", "rocket"]
+    
+    @IBOutlet private weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configCollectionView()
+    }
+
+    private func configCollectionView() {
+        collectionView.dataSource = self
+        collectionView.register(UINib(nibName: "PhotoCollectionViewCell", bundle: Bundle.main), forCellWithReuseIdentifier: "Cell")
+        let layout = PinterestLayout()
+        layout.delegate = self
+        collectionView.collectionViewLayout = layout
+    }
+}
+
+extension PhotoStreamViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! PhotoCollectionViewCell
+        cell.photoImageView.image = UIImage(named: images[indexPath.row])
+        return cell
+    }
+}
+
+extension PhotoStreamViewController: PinterestLayoutDelegate {
+    func collectionView(_ collectionView: UICollectionView, sizeForItemAtIndexPath indexPath: IndexPath) -> CGSize {
+        guard let image = UIImage(named: images[indexPath.row]) else { return .zero }
+        return image.size
+    }
+}
---
+++ 12_CollectionView/PhuocN/Baitap2/CollectionViewFlowLayout/CollectionViewFlowLayout/Views/Cells/PhotoCollectionViewCell 2.swift
@@ -0,0 +1,19 @@
+//
+//  PhotoCollectionViewCell.swift
+//  CollectionViewFlowLayout
+//
+//  Created by PhuocNguyen on 10/21/19.
+//  Copyright © 2019 PhuocNguyen. All rights reserved.
+//
+
+import UIKit
+
+final class PhotoCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        photoImageView.layer.cornerRadius = 15
+    }
+}
