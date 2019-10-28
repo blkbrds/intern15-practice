7c0ed1ec353d708e1329521c0b47f14dec128dd9
at-thongNguyen <thong.nguyen@asiantech.vn>
2019-10-28 16:46:48 +0700

    tạo các files bài tập phần MVC

---
+++ 08_MVC/ThongNT/BaiTapMVC/BaiTapMVC/Controllers/CalculatorViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorViewController.swift
+//  BaiTapMVC
+//
+//  Created by PCI0018 on 10/28/19.
+//  Copyright © 2019 Asian Tech. All rights reserved.
+//
+
+import UIKit
+
+final class CalculatorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ 08_MVC/ThongNT/BaiTapMVC/BaiTapMVC/Models/Results.swift
@@ -0,0 +1,9 @@
+//
+//  Results.swift
+//  BaiTapMVC
+//
+//  Created by PCI0018 on 10/28/19.
+//  Copyright © 2019 Asian Tech. All rights reserved.
+//
+
+import Foundation
