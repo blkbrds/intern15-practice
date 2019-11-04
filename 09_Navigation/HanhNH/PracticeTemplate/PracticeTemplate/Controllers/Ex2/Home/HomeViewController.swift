e378c5d5965a1d68b307a58a9edca78ff2976f76
ANH NGUYỄN <hanh.nguyen3@asiantech.vn>
2019-11-04 09:11:38 +0700

    fix loi cu phap

---
+++ 09_Navigation/HanhNH/PracticeTemplate/PracticeTemplate/Controllers/Ex2/Home/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  PracticeTemplate
+//
+//  Created by ANH NGUYỄN on 11/4/19.
+//  Copyright © 2019 Tien Le P. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
