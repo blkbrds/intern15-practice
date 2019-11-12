59f4ddff7726d3242a9db447dc8c2f06b135b887
Tien Tran V <tien.tran2@asiantech.vn>
2019-11-12 14:19:20 +0700

    Thay đổi thuật toán bài tập 2

--- 04_View/TienTranV/BaiTap02/BaiTap02/MyViewController.swift
+++ 04_View/TienTranV/BaiTap02/BaiTap02/MyViewController.swift
@@ -10,44 +10,65 @@ import UIKit
 
 class MyViewController: UIViewController {
 
-    let arrayName: [String] = ["CSS", "IOS", "Android", "Kotlin", "Java", "NodeJS", "Amazon", "MongoDB", "Swift"]
-    let arrayImage: [String] = ["girl-1.jpeg", "girl-2.jpeg", "girl-3.jpeg", "girl-4.jpeg", "girl-5.jpeg", "girl-6.jpeg", "girl-7.jpeg", "girl-8.jpeg", "girl-9.jpeg"]
+    let names: [String] = ["CSS", "IOS", "Android", "Kotlin", "Java", "NodeJS", "Amazon", "MongoDB", "Swift"]
+    let images: [String] = ["girl-1", "girl-2", "girl-3", "girl-4", "girl-5", "girl-6", "girl-7", "girl-8", "girl-9"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let space: CGFloat = 5
-        let widtthAvatarView = UIScreen.main.bounds.width / 3 - 1.5 * space
-        var heightAvatarView: CGFloat = 50
-        var count = 0
+        view.backgroundColor = .black
 
-        for _ in 0...2 {
-            for j in 0...2 {
-                // Add user avatar
-                let userAvatar = UIImageView(frame: CGRect(x: space + CGFloat(j) * (space + widtthAvatarView), y: heightAvatarView, width: widtthAvatarView, height: 150))
-                userAvatar.image = UIImage(named: arrayImage[count])
-                userAvatar.contentMode = .scaleAspectFit
-                view.addSubview(userAvatar)
+        let space: CGFloat = 10
+        let widthScreen = UIScreen.main.bounds.width
 
-                // Add user name
-                let userName = UILabel(frame: CGRect(x: space + CGFloat(j) * (space + widtthAvatarView), y: 150 + heightAvatarView, width: widtthAvatarView, height: 25))
-                userName.backgroundColor = .green
-                userName.text = arrayName[count]
-                userName.textAlignment = .center
-                view.addSubview(userName)
+        let widthUserView: CGFloat = 110
+        let heightUserView: CGFloat = 175
+        var yUserView: CGFloat = 50
+        var xUserView: CGFloat = space
 
-                // Add button
-                let button = UIButton(frame: CGRect(x: space + CGFloat(j) * (space + widtthAvatarView), y: heightAvatarView, width: widtthAvatarView, height: 175))
-                button.backgroundColor = .clear
-                let tapGesture = UITapGestureRecognizer(target: self, action: #selector(buttonDidClick))
-                tapGesture.name = arrayName[count]
-                button.addGestureRecognizer(tapGesture)
-                view.addSubview(button)
+        for index in 0..<names.count {
+            let frame = CGRect(x: xUserView, y: yUserView, width: widthUserView, height: heightUserView)
 
-                count += 1
+            let userView = createUserView(frame: frame, index: index)
+            self.view.addSubview(userView)
+
+            // Tinh frame
+            if xUserView + widthUserView > widthScreen - xUserView {
+                // Thay doi y va x
+                yUserView += heightUserView + space
+                xUserView = space
+            } else {
+                xUserView += widthUserView + space
             }
-            heightAvatarView += 175 + 10
         }
+
+    }
+
+    func createUserView(frame: CGRect, index: Int) -> UIView {
+        let userView = UIView(frame: frame)
+
+        //Add avatar
+        let avatar = UIImageView(frame: CGRect(x: 0, y: 0, width: userView.bounds.width, height: userView.bounds.height - 25))
+        avatar.image = UIImage(named: images[index])
+        avatar.contentMode = .scaleToFill
+        userView.addSubview(avatar)
+
+        //Add name
+        let name = UILabel(frame: CGRect(x: 0, y: 150, width: userView.bounds.width, height: userView.bounds.height - avatar.bounds.height))
+        name.text = names[index]
+        name.textAlignment = .center
+        name.backgroundColor = .orange
+        userView.addSubview(name)
+
+        //Add button --> bouns
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: userView.bounds.width, height: userView.bounds.height))
+        button.backgroundColor = .clear
+        let uiTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(buttonDidClick))
+        uiTapGestureRecognizer.name = names[index]
+        button.addGestureRecognizer(uiTapGestureRecognizer)
+        userView.addSubview(button)
+
+        return userView
     }
 
     @objc func buttonDidClick(_ sender: UITapGestureRecognizer) {
