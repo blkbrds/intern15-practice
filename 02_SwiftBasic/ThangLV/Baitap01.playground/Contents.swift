40f0ac3843f50b22193471e2b82a7ffe027f7b9f
Thang Le <thang.le@asiantech.vn>
2019-12-04 14:03:15 +0700

    Hoàn thành bài tập 1

---
+++ 02_SwiftBasic/ThangLV/Baitap01.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+func hinhCau(banKinh: Double) -> (dienTich: Double, theTich:Double){
+    let dienTich = 4*3.14*banKinh*banKinh
+    let theTich = (4/3)*3.14*banKinh*banKinh*banKinh
+    return (dienTich, theTich)
+}
+
+let hinhA = hinhCau(banKinh: 5)
+print("Hinh A co dien tich: \(hinhA.dienTich) va the tich: \(hinhA.theTich)")
