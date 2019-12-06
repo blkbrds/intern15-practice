f1deff65fd25a2b325f113725395ed5231053376
Hanh Nguyen H <hanh.nguyen3@asiantech.vn>
2019-12-06 23:08:55 +0700

    Đã làm xong BarButtonItem (Hoàn Thành phần UI)

--- 14_MVVM/HanhNH/Bai_Tap_14/Bai_Tap_14/View/Home/HomeViewController.swift
+++ 14_MVVM/HanhNH/Bai_Tap_14/Bai_Tap_14/View/Home/HomeViewController.swift
@@ -9,28 +9,44 @@
 import UIKit
 
 class HomeViewController: BaseViewController {
-    
+
     var address: [Address] = Address.getDummyDatas()
-    
+
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var tableView: UITableView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         configTableView()
         configCollectionView()
     }
-    
+
     override func setUpNaVi() {
         title = "Home"
+        let tableViewButton = UIBarButtonItem(image: UIImage(named: "tableViewMenu"), style: .plain, target: self, action: #selector(showTableView))
+        navigationItem.rightBarButtonItem = tableViewButton
     }
-    
+
+    @objc func showTableView() {
+        collectionView.isHidden = true
+        tableView.isHidden = false
+        let collectionViewButton = UIBarButtonItem(image: UIImage(named: "collectionMenu"), style: .plain, target: self, action: #selector(showCollectionView))
+        navigationItem.rightBarButtonItem = collectionViewButton
+    }
+
+    @objc func showCollectionView() {
+        collectionView.isHidden = false
+        tableView.isHidden = true
+        let tableViewButton = UIBarButtonItem(image: UIImage(named: "tableViewMenu"), style: .plain, target: self, action: #selector(showTableView))
+        navigationItem.rightBarButtonItem = tableViewButton
+    }
+
     func configTableView() {
         let nib = UINib(nibName: "HomeTableViewCell", bundle: Bundle.main)
         tableView.register(nib, forCellReuseIdentifier: "HomeTableViewCell")
         tableView.dataSource = self
     }
-    
+
     func configCollectionView() {
         let nib = UINib(nibName: "HomeCollectionViewCell", bundle: Bundle.main)
         collectionView.register(nib, forCellWithReuseIdentifier: "HomeCollectionViewCell")
@@ -44,7 +60,7 @@ extension HomeViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         address.count
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "HomeTableViewCell", for: indexPath) as! HomeTableViewCell
         cell.updateTabViewCell(avatar: address[indexPath.row].avatarImage, name: address[indexPath.row].nameImage, distance: address[indexPath.row].distance, value: address[indexPath.row].value, address: address[indexPath.row].address)
@@ -55,11 +71,11 @@ extension HomeViewController: UICollectionViewDataSource {
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         address.count
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "HomeCollectionViewCell", for: indexPath) as! HomeCollectionViewCell
         cell.updateCollectionView(image: address[indexPath.row].avatarImage, name: address[indexPath.row].nameImage, address: address[indexPath.row].address, distrance: address[indexPath.row].distance, value: address[indexPath.row].value)
