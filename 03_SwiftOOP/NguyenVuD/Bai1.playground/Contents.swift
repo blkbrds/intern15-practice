9d188d3f9d1b67bae1bb81872d18187521a9c111
at-nguyenvu <59037317+at-nguyenvu@users.noreply.github.com>
2019-12-26 11:38:56 +0700

    Update Contents.swift

--- 03_SwiftOOP/NguyenVuD/Bai1.playground/Contents.swift
+++ 03_SwiftOOP/NguyenVuD/Bai1.playground/Contents.swift
@@ -4,49 +4,56 @@ class PhanSo {
     var tuSo: Int
     var mauSo: Int
     
-    init(tuSo: Int, mauSo: Int) {
+    init?(tuSo: Int, mauSo: Int) {
+        if mauSo == 0 {
+            return nil
+        }
         self.tuSo = tuSo
         self.mauSo = mauSo
     }
     
-    func xuat(){
+    func xuat() {
         print("\(tuSo) / \(mauSo)")
     }
     
-    func congHaiPhanSo(phanSo2: PhanSo) -> PhanSo {
+    func congHaiPhanSo(phanSo2: PhanSo?) -> PhanSo? {
+        guard let phanSo2 = phanSo2 else { return nil }
         let tu = tuSo * phanSo2.mauSo + mauSo * phanSo2.tuSo
         let mau = mauSo * phanSo2.mauSo
         return PhanSo(tuSo: tu, mauSo: mau)
     }
     
-    func truHaiPhanSo(phanSo2: PhanSo) -> PhanSo {
+    func truHaiPhanSo(phanSo2: PhanSo?) -> PhanSo? {
+        guard let phanSo2 = phanSo2 else { return nil }
         let tu = tuSo * phanSo2.mauSo - mauSo * phanSo2.tuSo
         let mau = mauSo * phanSo2.mauSo
         return PhanSo(tuSo: tu, mauSo: mau)
     }
     
-    func nhanHaiPhanSo(phanSo2: PhanSo) -> PhanSo {
+    func nhanHaiPhanSo(phanSo2: PhanSo?) -> PhanSo? {
+        guard let phanSo2 = phanSo2 else { return nil }
         let tu = tuSo * phanSo2.tuSo
         let mau = mauSo * phanSo2.mauSo
         return PhanSo(tuSo: tu, mauSo: mau)
     }
     
-    func chiaHaiPhanSo(phanSo2: PhanSo) -> PhanSo {
+    func chiaHaiPhanSo(phanSo2: PhanSo?) -> PhanSo? {
+        guard let phanSo2 = phanSo2 else { return nil }
         let tu = tuSo * phanSo2.mauSo
         let mau = mauSo * phanSo2.tuSo
         return PhanSo(tuSo: tu, mauSo: mau)
     }
 }
 
-var phanSoA = PhanSo(tuSo: 2, mauSo: 3)
-var phanSoB = PhanSo(tuSo: 3, mauSo: 2)
+var phanSoA = PhanSo(tuSo: 2, mauSo: 0)
+var phanSoB = PhanSo(tuSo: 3, mauSo: 0)
 
-var cong = phanSoA.congHaiPhanSo(phanSo2: phanSoB)
-var tru = phanSoA.truHaiPhanSo(phanSo2: phanSoB)
-var nhan = phanSoA.nhanHaiPhanSo(phanSo2: phanSoB)
-var chia = phanSoA.chiaHaiPhanSo(phanSo2: phanSoB)
+var cong = phanSoA?.congHaiPhanSo(phanSo2: phanSoB)
+var tru = phanSoA?.truHaiPhanSo(phanSo2: phanSoB)
+var nhan = phanSoA?.nhanHaiPhanSo(phanSo2: phanSoB)
+var chia = phanSoA?.chiaHaiPhanSo(phanSo2: phanSoB)
 
-print("\(phanSoA.xuat()) + \(phanSoB.xuat()) = \(cong.xuat())")
-print("\(phanSoA.xuat()) - \(phanSoB.xuat()) = \(tru.xuat())")
-print("\(phanSoA.xuat()) * \(phanSoB.xuat()) = \(nhan.xuat())")
-print("\(phanSoA.xuat()) / \(phanSoB.xuat()) = \(chia.xuat())")
+print("\(phanSoA?.xuat()) + \(phanSoB?.xuat()) = \(cong?.xuat())")
+print("\(phanSoA?.xuat()) - \(phanSoB?.xuat()) = \(cong?.xuat())")
+print("\(phanSoA?.xuat()) * \(phanSoB?.xuat()) = \(cong?.xuat())")
+print("\(phanSoA?.xuat()) / \(phanSoB?.xuat()) = \(cong?.xuat())")
