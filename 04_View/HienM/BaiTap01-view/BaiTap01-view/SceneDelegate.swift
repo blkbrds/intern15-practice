cd6a37e9ab87c662974cbc7383ce1057386a4542
at-hienmai <hien.mai@asiantech.vn>
2019-12-26 11:36:55 +0700

    Hoan thanh  Bai 1, 2  - view

---
+++ 04_View/HienM/BaiTap01-view/BaiTap01-view/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  BaiTap01-view
+//
+//  Created by Ngoc Hien on 12/26/19.
+//  Copyright © 2019 NgocHien. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+	var window: UIWindow?
+
+	func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = MyViewController()
+        self.window = window
+        window.makeKeyAndVisible()
+        
+    }
+}
---
+++ 04_View/HienM/BaiTap01-view/BaiTap01-view/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BaiTap01-view
+//
+//  Created by Ngoc Hien on 12/26/19.
+//  Copyright © 2019 NgocHien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
