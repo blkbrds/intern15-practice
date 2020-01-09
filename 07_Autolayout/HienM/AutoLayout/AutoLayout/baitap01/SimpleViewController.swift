8ab305ac7ac4700615aa1302388d6bdd3c4484b6
at-hienmai <hien.mai@asiantech.vn>
2020-01-09 22:40:03 +0700

    no message

---
+++ 07_Autolayout/HienM/AutoLayout/AutoLayout/baitap01/SimpleViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SimpleViewController.swift
+//  AutoLayout
+//
+//  Created by Ngoc Hien on 1/9/20.
+//  Copyright © 2020 NgocHien. All rights reserved.
+//
+
+import UIKit
+
+class SimpleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ 07_Autolayout/HienM/AutoLayout/AutoLayout/baitap03/Simple3ViewController.swift
@@ -0,0 +1,10 @@
+
+import UIKit
+
+class Simple3ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
