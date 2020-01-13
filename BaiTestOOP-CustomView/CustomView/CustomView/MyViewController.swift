6ff230fae30b6d8e6ffaa4593984012a3aca2d17
at-hienmai <hien.mai@asiantech.vn>
2020-01-13 13:24:24 +0700

    no message

--- BaiTestOOP-CustomView/CustomView/CustomView/MyViewController.swift
+++ BaiTestOOP-CustomView/CustomView/CustomView/MyViewController.swift
@@ -5,9 +5,9 @@ class MyViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		
+
 		let userView = Bundle.main.loadNibNamed("FavoriteView", owner: self, options: nil)?.first as? FavoriteView
-		let frame =  CGRect(x: 0, y: 300, width:255, height: 388)
+		let frame = CGRect(x: 80, y: 300, width: 255, height: 388)
 		userView?.frame = frame
 		view.addSubview(userView!)
 	}
