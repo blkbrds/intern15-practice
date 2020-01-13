5ee216390d1af95b8c45986ec947dc23f01e1b5b
at-hienmai <hien.mai@asiantech.vn>
2020-01-14 00:55:23 +0700

    no message

--- BaiTestOOP-CustomView/CustomView/CustomView/FavoriteView.swift
+++ BaiTestOOP-CustomView/CustomView/CustomView/FavoriteView.swift
@@ -1,7 +1,18 @@
 import SwiftUI
-class FavoriteView: UIView {
-
+protocol UserViewDelegate {
+	func didTap(view: FavoriteView
+	)
 }
+class FavoriteView: UIView {
+	var delegate: UserViewDelegate?
+
+	@IBAction func closeButton(_ sender: Any) {
+		self.isHidden = true
+	}
+}
+
+
+
 
 
 
--- BaiTestOOP-CustomView/CustomView/CustomView/MyViewController.swift
+++ BaiTestOOP-CustomView/CustomView/CustomView/MyViewController.swift
@@ -2,14 +2,36 @@
 import UIKit
 
 class MyViewController: UIViewController {
-
+	var favoriteView: FavoriteView!
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
-		let userView = Bundle.main.loadNibNamed("FavoriteView", owner: self, options: nil)?.first as? FavoriteView
-		let frame = CGRect(x: 80, y: 300, width: 255, height: 388)
-		userView?.frame = frame
-		view.addSubview(userView!)
+	}
+	override func viewDidAppear(_ animated: Bool) {
+		setupUI()
+		favoriteView.delegate = self
 	}
 
+	private func setupUI() {
+		favoriteView = Bundle.main.loadNibNamed("FavoriteView", owner: self, options: nil)?.first as? FavoriteView
+		let frame = CGRect(x: 40, y: 150, width: 300, height: 400)
+		favoriteView.frame = frame
+		favoriteView.layer.cornerRadius = 10
+		if let subview = favoriteView  {
+			view.addSubview(subview)
+		}
+	}
+	
+	@IBAction func actionButton(_ sender: Any) {
+		favoriteView?.isHidden = true
+	}
+	
+	@IBAction func favoriteAction(_ sender: Any) {
+		favoriteView?.isHidden = true
+	}
+}
+
+extension MyViewController: UserViewDelegate {
+	func didTap(view: FavoriteView) {
+	}
 }
