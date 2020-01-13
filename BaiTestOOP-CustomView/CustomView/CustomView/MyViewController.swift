b81ce424564ce6a930a1c78eba3f9d18ba8832d7
at-hienmai <hien.mai@asiantech.vn>
2020-01-13 11:53:41 +0700

    Test CustomView

---
+++ BaiTestOOP-CustomView/CustomView/CustomView/MyViewController.swift
@@ -0,0 +1,15 @@
+
+import UIKit
+
+class MyViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		let userView = Bundle.main.loadNibNamed("FavoriteView", owner: self, options: nil)?.first as? FavoriteView
+		let frame =  CGRect(x: 0, y: 300, width:255, height: 388)
+		userView?.frame = frame
+		view.addSubview(userView!)
+	}
+
+}
