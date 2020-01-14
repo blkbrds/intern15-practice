41aa3b4131e03bd145fe582a1a0f6d78e4bd935e
at-hienmai <hien.mai@asiantech.vn>
2020-01-14 14:09:43 +0700

    no message

--- 07_Autolayout/HienM/AutoLayout/AutoLayout/SceneDelegate.swift
+++ 07_Autolayout/HienM/AutoLayout/AutoLayout/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = StackViewController()
+        window.rootViewController = CalculatorViewController()
         self.window = window
         window.makeKeyAndVisible()
     }
--- 07_Autolayout/HienM/AutoLayout/AutoLayout/baitap10/SquareViewController.swift
+++ 07_Autolayout/HienM/AutoLayout/AutoLayout/baitap10/SquareViewController.swift
@@ -13,13 +13,10 @@ class SquareViewController: UIViewController {
 	let CGsizeBig = CGSize(width: 160, height: 160)
 	let CGsizeSmall = CGSize(width: 80, height: 80)
 
-	@IBAction func actionPinkButton(_ sender: Any) {
-
-	self.view.addConstraint(NSLayoutConstraint(item: pinkButton as Any, attribute: NSLayoutConstraint.Attribute.leading, relatedBy: NSLayoutConstraint.Relation.equal, toItem: blueButton, attribute: NSLayoutConstraint.Attribute.leading, multiplier: 1.0, constant: 0.0))
-//
-		//self.view.addConstraint(NSLayoutConstraint(item: blueButton as Any, attribute: NSLayoutConstraint.Attribute.trailing, relatedBy: NSLayoutConstraint.Relation.equal, toItem: pinkButton, attribute: NSLayoutConstraint.Attribute.trailing, multiplier: 1.0, constant: 0.0))
-//
-		//	self.view.addConstraint(NSLayoutConstraint(item: pinkButton as Any, attribute: NSLayoutConstraint.Attribute.bottom, relatedBy: NSLayoutConstraint.Relation.equal, toItem: blueButton, attribute: NSLayoutConstraint.Attribute.bottom, multiplier: 1.0, constant: 0.0))
+	@IBAction func actionPinkButton(_ sender: UIButton) {
+		UIView.animate(withDuration: 1) {
+			
+		}
 	}
 
 }
