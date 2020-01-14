22c815d05e68d61621ca1749f65b89a8a6898c46
at-hienmai <hien.mai@asiantech.vn>
2020-01-14 09:15:17 +0700

    no message

--- BaiTestOOP-CustomView/CustomView/CustomView/MyViewController.swift
+++ BaiTestOOP-CustomView/CustomView/CustomView/MyViewController.swift
@@ -2,9 +2,14 @@
 import UIKit
 
 class MyViewController: UIViewController {
+	@IBOutlet weak var unfavoriteButton: UIButton!
+	@IBOutlet weak var favoriteButton: UIButton!
+
 	var favoriteView: FavoriteView!
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		favoriteButton.layer.cornerRadius = 20
+		unfavoriteButton.layer.cornerRadius = 20
 
 	}
 	override func viewDidAppear(_ animated: Bool) {
@@ -14,18 +19,18 @@ class MyViewController: UIViewController {
 
 	private func setupUI() {
 		favoriteView = Bundle.main.loadNibNamed("FavoriteView", owner: self, options: nil)?.first as? FavoriteView
-		let frame = CGRect(x: 40, y: 150, width: 300, height: 400)
+		let frame = CGRect(x: 40, y: 150, width: 300, height: 350)
 		favoriteView.frame = frame
 		favoriteView.layer.cornerRadius = 10
-		if let subview = favoriteView  {
+		if let subview = favoriteView {
 			view.addSubview(subview)
 		}
 	}
-	
+
 	@IBAction func actionButton(_ sender: Any) {
 		favoriteView?.isHidden = true
 	}
-	
+
 	@IBAction func favoriteAction(_ sender: Any) {
 		favoriteView?.isHidden = true
 	}
