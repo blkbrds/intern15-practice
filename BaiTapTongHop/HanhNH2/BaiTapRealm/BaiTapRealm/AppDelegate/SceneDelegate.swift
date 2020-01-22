99d8d628c1f44f9e1147d11402fb16c29a94e277
ANH NGUYỄN <hanh.nguyen3@asiantech.vn>
2020-01-22 14:12:00 +0700

    Revise the TabBar section As required.

--- BaiTapTongHop/HanhNH2/BaiTapRealm/BaiTapRealm/AppDelegate/SceneDelegate.swift
+++ BaiTapTongHop/HanhNH2/BaiTapRealm/BaiTapRealm/AppDelegate/SceneDelegate.swift
@@ -17,13 +17,41 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         let window = UIWindow(windowScene: windowScene)
         self.window = window
         window.makeKeyAndVisible()
-        setRootTabbar()
+
+        createTabbar()
+    }
+
+    private func createTabbar() {
+        //Home
+        let homeVC = HomeViewController()
+        let homeNavi = UINavigationController(rootViewController: homeVC)
+        homeNavi.tabBarItem = UITabBarItem(title: "Home", image: #imageLiteral(resourceName: "ic-tabbar-home"), tag: 0)
+
+        //Messages
+        let mapVC = MapViewController()
+        let mapNavi = UINavigationController(rootViewController: mapVC)
+        mapNavi.tabBarItem = UITabBarItem(title: "Map", image: #imageLiteral(resourceName: "ic-tabbar-map"), tag: 1)
+
+        //Friends
+        let favoriteVC = FavoriteViewController()
+        let favoriteNavi = UINavigationController(rootViewController: favoriteVC)
+        favoriteNavi.tabBarItem = UITabBarItem(title: "Favorite", image: #imageLiteral(resourceName: "ic-tabbar-favorites"), tag: 2)
+
+        //Profile
+        let profileVC = ProfileViewController()
+        let profileNavi = UINavigationController(rootViewController: profileVC)
+        profileNavi.tabBarItem = UITabBarItem(title: "Favorite", image: #imageLiteral(resourceName: "ic-tabbar-profile"), tag: 3)
+
+        //tabbar controller
+        let tabbarController = UITabBarController()
+        tabbarController.viewControllers = [homeNavi, mapNavi, favoriteNavi, profileNavi]
+        tabbarController.tabBar.tintColor = .black
+
+        guard let window = window else { return }
+        window.rootViewController = tabbarController
     }
 }
-extension SceneDelegate {
-    private func setRootTabbar() {
-        let tabbarVC = BaseTabBarController()
-        window?.rootViewController = tabbarVC
-    }
-}
+
+
+
 
