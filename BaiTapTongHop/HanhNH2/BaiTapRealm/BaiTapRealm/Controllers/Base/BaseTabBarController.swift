f2bc456cb1e762a9b40dc13144a079a9771d70d1
ANH NGUYỄN <hanh.nguyen3@asiantech.vn>
2020-01-22 10:31:37 +0700

    create View Model for TabBarController.

--- BaiTapTongHop/HanhNH2/BaiTapRealm/BaiTapRealm/Controllers/Base/BaseTabBarController.swift
+++ BaiTapTongHop/HanhNH2/BaiTapRealm/BaiTapRealm/Controllers/Base/BaseTabBarController.swift
@@ -9,44 +9,8 @@
 import UIKit
 
 final class BaseTabBarController: UITabBarController {
-    enum TabbarType: Int {
-        case home
-        case map
-        case favorite
-        case profile
-
-        var title: String {
-            switch self {
-            case .home:
-                return "Home"
-            case .map:
-                return "Map"
-            case .favorite:
-                return "Favorite"
-            case .profile:
-                return "Profile"
-            }
-        }
-
-        var imageName: String {
-            switch self {
-            case .home:
-                return "ic-tabbar-home"
-            case .map:
-                return "ic-tabbar-map"
-            case .favorite:
-                return "ic-tabbar-favorites"
-            case .profile:
-                return "ic-tabbar-profile"
-            }
-        }
-    }
-
-
-    private var homeNavi = UINavigationController(rootViewController: HomeViewController())
-    private var mapNaVi = UINavigationController(rootViewController: MapViewController())
-    private var favoriteNaVi = UINavigationController(rootViewController: FavoriteViewController())
-    private var profileNaVi = UINavigationController(rootViewController: ProfileViewController())
+    
+    var viewModel = BaseTabBarViewModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -54,16 +18,19 @@ final class BaseTabBarController: UITabBarController {
     }
 
     private func configTabBar() {
-        homeNavi.tabBarItem = getTabbarItem(for: .home)
-        mapNaVi.tabBarItem = getTabbarItem(for: .map)
-        favoriteNaVi.tabBarItem = getTabbarItem(for: .favorite)
-        profileNaVi.tabBarItem = getTabbarItem(for: .profile)
+        let homeNavi = UINavigationController(rootViewController: HomeViewController())
+        let mapNaVi = UINavigationController(rootViewController: MapViewController())
+        let favoriteNaVi = UINavigationController(rootViewController: FavoriteViewController())
+        let profileNaVi = UINavigationController(rootViewController: ProfileViewController())
+        let viewControllers = [homeNavi, mapNaVi, favoriteNaVi, profileNaVi]
+        for (index, vc) in viewControllers.enumerated() {
+            let tabbarType = viewModel.getTabType(at: index)
+            vc.tabBarItem = UITabBarItem(title: tabbarType.title,
+                                         image: #imageLiteral(resourceName: tabbarType.imageName),
+                                         tag: tabbarType.rawValue)
+        }
 
-        setViewControllers([homeNavi, mapNaVi, favoriteNaVi, profileNaVi], animated: true)
+        setViewControllers(viewControllers, animated: true)
         tabBar.tintColor = .black
-    }
-
-    private func getTabbarItem(for tabbarType: TabbarType) -> UITabBarItem {
-        UITabBarItem(title: tabbarType.title, image: #imageLiteral(resourceName: tabbarType.imageName), tag: tabbarType.rawValue)
     }
 }
