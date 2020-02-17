79b5022eb244e5ca31df2d2d2512d7b17c1ea3c2
Vien Hoang <vien.hoang@asiantech.vn>
2020-02-17 13:43:56 +0700

    fix comment

--- 13_TabbarController/VienH/baitap13/baitap13/Resource/SceneDelegate.swift
+++ 13_TabbarController/VienH/baitap13/baitap13/Resource/SceneDelegate.swift
@@ -24,7 +24,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         self.window = window
         window.makeKeyAndVisible()
 
-        changeScreen(type: .tabbar)
+        changeScreen(type: .login)
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
