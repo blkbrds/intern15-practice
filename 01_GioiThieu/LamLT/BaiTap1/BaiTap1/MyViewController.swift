b872266b71443339ec0d2b20aa68e6a36f3c04b5
Lam Le <lam.le2@asiantech.vn>
2020-02-24 17:23:25 +0700

    Fix Comment

--- 01_GioiThieu/LamLT/BaiTap1/BaiTap1/MyViewController.swift
+++ 01_GioiThieu/LamLT/BaiTap1/BaiTap1/MyViewController.swift
@@ -7,17 +7,9 @@ class MyViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.white
-        // Do any additional setup after loading the view.
     }
-
-
-
- 
-
+    
     @IBAction func changeColorButtonTouchUpInside(_ sender: Any) {
-//        let random = CGFloat.random(in: 0...255) / 255
-//        view.backgroundColor = UIColor(displayP3Red: random, green: random, blue: random, alpha: random )
         self.view.backgroundColor = UIColor(displayP3Red: CGFloat.random(in: 0...255) / 255, green: CGFloat.random(in: 0...255) / 255, blue: CGFloat.random(in: 0...255) / 255, alpha: 1)
-
     }
 }
