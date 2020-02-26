236cd07e6592eb07852f166b8396fac03772cc81
Lam Le <lam.le2@asiantech.vn>
2020-02-26 10:06:35 +0700

    Fix Comment

--- 02_SwiftBasic/LamLT/Bai3.playground/Contents.swift
+++ 02_SwiftBasic/LamLT/Bai3.playground/Contents.swift
@@ -1,24 +1,22 @@
 import UIKit
 
-class Equation2Grace{
-    func solveEquation2Grace(a1: Float,a2: Float,b1: Float,b2: Float,c1: Float,c2: Float){
-        var x: Float,y: Float
+class Equation2Grace {
+    func solveEquation2Grace(a1: Float, a2: Float, b1: Float, b2: Float, c1: Float, c2: Float) {
+        var x: Float, y: Float
         let d: Float = a1 * b2 - a2 * b1
         let d1: Float = c1 * b2 - c2 * b1
         let d2: Float = a1 * c2 - a2 * c1
-        
+
         if d == 0 {
-            if d1 == d2{
+            if d1 == d2 {
                 print("Quation has infiniti solution")
-            }
-            else{
+            } else {
                 print("Quation has no solution")
             }
-        }
-        else{
+        } else {
             print("Quation has one solution")
-            x = d1/d
-            y = d2/d
+            x = d1 / d
+            y = d2 / d
             print("x = \(x) y = \(y)")
         }
     }
