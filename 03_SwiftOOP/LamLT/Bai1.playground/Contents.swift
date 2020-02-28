6b462e62afaf1bd69158d44837674c4cb43d41e4
Lam Le <lam.le2@asiantech.vn>
2020-02-28 15:36:18 +0700

    Fix Comment

--- 03_SwiftOOP/LamLT/Bai1.playground/Contents.swift
+++ 03_SwiftOOP/LamLT/Bai1.playground/Contents.swift
@@ -8,8 +8,8 @@ class Fraction {
         self.numerator = numerator
         self.denominator = denominator
     }
-    
-    func UCLN(a: Int,b: Int) -> Int {
+
+    func UCLN(a: Int, b: Int) -> Int {
         var x: Int = a
         var y: Int = b
         while x != y {
@@ -21,41 +21,49 @@ class Fraction {
         }
         return x
     }
-    
-    func minimalistFraction(a1: Int,b1: Int){
+
+    func minimalistFraction(a1: Int, b1: Int) -> String {
         let i = UCLN(a: a1, b: b1)
         let a2: Int = a1 / i
         let b2: Int = b1 / i
-        print("\(a2) / \(b2)")
+        return "\(a2) / \(b2)"
     }
-    
-    func congPhanSo(f: Fraction){
+
+    func congPhanSo(f: Fraction) -> String {
         let ts: Int = numerator * f.denominator + f.numerator * denominator
         let ms: Int = denominator * f.denominator
         let result = minimalistFraction(a1: ts, b1: ms)
+        return result
     }
-    
-    func truPhanSo(f: Fraction){
+
+    func truPhanSo(f: Fraction) -> String {
         let ts: Int = numerator * f.denominator - f.numerator * denominator
         let ms: Int = denominator * f.denominator
         let result = minimalistFraction(a1: ts, b1: ms)
+        return result
     }
-    
-    func nhanPhanSo(f: Fraction){
+
+    func nhanPhanSo(f: Fraction) -> String {
         let ts: Int = numerator * f.numerator
         let ms: Int = denominator * f.denominator
         let result = minimalistFraction(a1: ts, b1: ms)
+        return result
     }
-    
-    func chiaPhanSo(f: Fraction){
+
+    func chiaPhanSo(f: Fraction) -> String {
         let ts: Int = numerator * f.denominator
         let ms: Int = denominator * f.numerator
         let result = minimalistFraction(a1: ts, b1: ms)
+        return result
     }
 }
 let frac1 = Fraction(numerator: 15, denominator: 5)
 let frac2 = Fraction(numerator: 5, denominator: 10)
 var congTest = frac1.congPhanSo(f: frac2)
+print(congTest)
 var truTest = frac1.truPhanSo(f: frac2)
+print(truTest)
 var nhanTest = frac1.nhanPhanSo(f: frac2)
+print(nhanTest)
 var chiaTest = frac1.chiaPhanSo(f: frac2)
+print(chiaTest)
