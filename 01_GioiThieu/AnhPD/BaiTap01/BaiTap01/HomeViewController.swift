8f4216d98876e651a39631075be992e670c3c200
Anh Pham D.[2] <anh.pham2@asiantech.vn>
2020-03-05 16:06:07 +0700

    Hoàn thành bài tập 1

---
+++ 01_GioiThieu/AnhPD/BaiTap01/BaiTap01/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  BaiTap01
+//
+//  Created by PCI0012 on 3/5/20.
+//  Copyright © 2020 PCI0012. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    let arrayColor = [UIColor.red, UIColor.white, UIColor.green, UIColor.yellow,UIColor.blue, UIColor.brown, UIColor.black]
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+    @IBAction func tap(_ sender: Any) {
+        print("Tap")
+        let index = Int.random(in: 0...6)
+        view.backgroundColor = arrayColor[index]
+    }
+    
+  
+
+}
---
+++ 01_GioiThieu/AnhPD/BaiTap01/BaiTap01Tests/BaiTap01Tests.swift
@@ -0,0 +1,34 @@
+//
+//  BaiTap01Tests.swift
+//  BaiTap01Tests
+//
+//  Created by PCI0012 on 3/5/20.
+//  Copyright © 2020 PCI0012. All rights reserved.
+//
+
+import XCTest
+@testable import BaiTap01
+
+class BaiTap01Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
