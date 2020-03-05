9743fc32483c6f7ba4edb056f43f7be3f8157cac
Quan Bui T <quan.bui@asiantech.vn>
2020-03-05 16:23:51 +0700

    fix comment

--- 01_GioiThieu/QuanBT/Baitap01/Baitap01/ViewController.swift
+++ 01_GioiThieu/QuanBT/Baitap01/Baitap01/ViewController.swift
@@ -1,24 +1,14 @@
-
-
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
     
     @IBOutlet weak var tapButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-//        tapButton.addTarget(self, action: #selector(buttonrandom), for: .touchUpInside)
-        
-        
-    }
-    
-    @objc func buttonrandom() {
-        view.backgroundColor = UIColor(displayP3Red: CGFloat.random(in: 0...255) / 255, green: CGFloat.random(in: 0...255) / 255, blue: CGFloat.random(in: 0...255) / 255, alpha: 1)
     }
     
     @IBAction func random(_ sender: Any) {
          view.backgroundColor = UIColor(displayP3Red: CGFloat.random(in: 0...255) / 255, green: CGFloat.random(in: 0...255) / 255, blue: CGFloat.random(in: 0...255) / 255, alpha: 1)
     }
-    
-    
 }
