1012780da1772ac02ed11d92f7028479635e0052
at-thinhnguyen6 <thinh.nguyen@asiantech.vn>
2020-03-07 22:04:08 +0700

    sua ten bien

--- 01_GioiThieu/ThinhN/Baitap1/Baitap1/Home/MyViewController.swift
+++ 01_GioiThieu/ThinhN/Baitap1/Baitap1/Home/MyViewController.swift
@@ -16,10 +16,10 @@ class MyViewController: UIViewController {
     }
     
     @IBAction func tapButton(_ sender: Any) {
-    let number = Int.random(in: 0...255 )
+    let number0 = Int.random(in: 0...255 )
     let number1 = Int.random(in: 0...255)
     let number2 = Int.random(in: 0...255 )
-    let color = UIColor(displayP3Red: CGFloat(number)/255, green: CGFloat(number1)/255, blue: CGFloat(number2)/255, alpha: 1)
+    let color = UIColor(displayP3Red: CGFloat(number0)/255, green: CGFloat(number1)/255, blue: CGFloat(number2)/255, alpha: 1)
     view.backgroundColor = color
     }
     
