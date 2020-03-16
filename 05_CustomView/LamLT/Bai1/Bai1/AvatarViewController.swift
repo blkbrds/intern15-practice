284565527cbb0912bb93cf81ce24666ce5b0b87c
Lam Le <lam.le2@asiantech.vn>
2020-03-16 09:00:33 +0700

    no message

--- 05_CustomView/LamLT/Bai1/Bai1/AvatarViewController.swift
+++ 05_CustomView/LamLT/Bai1/Bai1/AvatarViewController.swift
@@ -4,7 +4,7 @@ class AvatarViewController: UIViewController {
 
     var xSpacing = 20
     var ySpaceing = 30
-    var name = ["Jack", "SISO", "TIDE", "OBAMA", "OMO", "SIRO", "TURTEL", "JEANNE", "JESSI"]
+    var names = ["Jack", "SISO", "TIDE", "OBAMA", "OMO", "SIRO", "TURTEL", "JEANNE", "JESSI"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -13,11 +13,11 @@ class AvatarViewController: UIViewController {
         let screenHeight = screenSize.height
         let myScrollView = UIScrollView(frame: CGRect(x: 10, y: 10, width: screenWidth, height: screenHeight))
         view.addSubview(myScrollView)
-        for i in 1..<name.count + 1 {
+        for i in 1..<names.count + 1 {
             let frame = CGRect(x: xSpacing, y: ySpaceing, width: 100, height: 200)
             let avatarView = AvatarView(frame: frame)
             avatarView.delegate = self
-            avatarView.userNameLabel?.text = name[i - 1]
+            avatarView.userNameLabel?.text = names[i - 1]
             xSpacing += 110
             if i % 3 == 0 {
                 xSpacing = 20
