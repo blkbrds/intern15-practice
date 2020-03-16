daf567c909f26c03f479dad148977513e0f82c24
Lam Le <lam.le2@asiantech.vn>
2020-03-16 09:35:32 +0700

    test Pull

--- 06_Drawing/LamLT/Bai1/Bai1/ChartViewController.swift
+++ 06_Drawing/LamLT/Bai1/Bai1/ChartViewController.swift
@@ -2,13 +2,12 @@ import UIKit
 
 class ChartViewController: UIViewController {
     var ballColor: UIColor = .green
-    
-    var x: Int = 1
+
     override func viewDidLoad() {
         super.viewDidLoad()
         drawLine(start: CGPoint(x: 50, y: 100), end: CGPoint(x: 50, y: 500))
         drawLine(start: CGPoint(x: 50, y: 500), end: CGPoint(x: 500, y: 500))
-        
+
         drawChart(start: CGPoint(x: 50, y: 440), end: CGPoint(x: 100, y: 400))
         drawChart(start: CGPoint(x: 100, y: 400), end: CGPoint(x: 150, y: 320))
         drawChart(start: CGPoint(x: 150, y: 320), end: CGPoint(x: 200, y: 270))
@@ -20,9 +19,9 @@ class ChartViewController: UIViewController {
         createLabel(xPosition: 2, yPosition: 430, textLine: "999")
         createLabel(xPosition: 2, yPosition: 280, textLine: "1000")
         createLabel(xPosition: 2, yPosition: 150, textLine: "1001")
-        
-        drawCircle(radius: 9, xPosArr: [50,100,150,200,250,300,350], yPosArr: [440,400,320,270,300,264,360])
-        
+
+        drawCircle(radius: 9, xPosArr: [50, 100, 150, 200, 250, 300, 350], yPosArr: [440, 400, 320, 270, 300, 264, 360])
+
     }
 
     func drawLine(start: CGPoint, end: CGPoint) {
@@ -55,7 +54,7 @@ class ChartViewController: UIViewController {
         self.view.layer.addSublayer(shapeLayer)
     }
 
-    func drawCircle(radius: CGFloat,xPosArr: [CGFloat],yPosArr: [CGFloat]) {
+    func drawCircle(radius: CGFloat, xPosArr: [CGFloat], yPosArr: [CGFloat]) {
         for i in 0..<xPosArr.count {
             let frame = CGRect(x: xPosArr[i], y: yPosArr[i], width: radius, height: radius)
             let circle = ChartView(frame: frame)
@@ -63,7 +62,7 @@ class ChartViewController: UIViewController {
             self.view.addSubview(circle)
         }
     }
-    
+
     func createLabel(xPosition: CGFloat, yPosition: CGFloat, textLine: String) {
         let label = UILabel()
         label.frame = CGRect(x: xPosition, y: yPosition, width: 50, height: 20)
