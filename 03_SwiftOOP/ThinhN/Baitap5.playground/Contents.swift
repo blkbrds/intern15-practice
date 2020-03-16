dfd0a267a117ab16273caf48a3e8a4493244dddd
Thinh Nguyen P .[6] <thinh.nguyen6@asiantech.vn>
2020-03-16 19:29:55 +0700

    Hoàn thành bài 7, cập nhật bài 5

--- 03_SwiftOOP/ThinhN/Baitap5.playground/Contents.swift
+++ 03_SwiftOOP/ThinhN/Baitap5.playground/Contents.swift
@@ -1,34 +1,57 @@
 import UIKit
 
-class CStack {
+class CStack{
+    var mangSoNguyen :[Int]
     var soStack :Int
-    var Stack : [Int]
-    
-    init(themStack :Int, Stack: [Int]){
-    self.soStack = themStack
-    self.Stack = Stack
+    init(soStack:Int, mangSoNguyen :[Int]){
+        self.mangSoNguyen = mangSoNguyen
+        self.soStack = soStack
+        if self.soStack < self.mangSoNguyen.count{
+            print (" Stack khong hop le ")
+        }
     }
     
     func emptyStack(){
-        if self.Stack.count == 0{
-        print ( " Stack rỗng")
-        } else {
-            print("Stack có chứa giá trị")
+        if self.mangSoNguyen.isEmpty{
+            print(" Stack rong")
+        }else{
+            print("Stack co chua gia tri")
         }
     }
-        
-    func fullStack(){
-        if self.Stack.count == soStack {
-            print(" Stack full")
-        } else {
-            print("Stack chưa đầy ")
-        }
-    }
-    func addStack(){
-        self.Stack.append(soStack)
-            print(" Da them 1 stack")
-        
-        }
-}
-
     
+    func fullStack(){
+        if mangSoNguyen.count == soStack {
+            print (" Stack day gia tri ")
+        }else{
+            print ("Stack khong day gia tri")
+        }
+    }
+    
+   func addStack(them: Int){
+    if mangSoNguyen.count == soStack {
+        print (" khong the them stack duoc")
+    }else{
+        mangSoNguyen.append(them)
+    }
+    }
+    func delStack(){
+        if mangSoNguyen.count == 0{
+          print("khong the xoa them ")
+        }else{
+         self.mangSoNguyen.removeLast()
+   }
+}
+}
+var mangStack = CStack.init(soStack: 5,mangSoNguyen:[1,3,4,7,5])
+
+mangStack.emptyStack()
+mangStack.fullStack()
+print(mangStack.mangSoNguyen)
+mangStack.addStack(them: 7)
+print(mangStack.mangSoNguyen)
+mangStack.addStack(them: 8)
+print(mangStack.mangSoNguyen)
+mangStack.addStack(them: 12)
+print(mangStack.mangSoNguyen)
+mangStack.delStack()
+print(mangStack.mangSoNguyen)
