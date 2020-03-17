8cfc3eaac131aeb635f5d516753fd46242018c33
Lam Le <lam.le2@asiantech.vn>
2020-03-17 16:51:22 +0700

    Up bài 1 2 Layout

---
+++ 07_Autolayout/LamLT/Bai1/Bai1/ViewController.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var labelView: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        labelView.transform = CGAffineTransform(scaleX: 2, y: 2)
+//        labelView.transform = CGAffineTransform(translationX: -256, y: -256)
+//        labelView.transform =
+    }
+
+
+}
