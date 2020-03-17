479e8656954980b44125b27af39f07ac29959dec
Thinh Nguyen P .[6] <thinh.nguyen6@asiantech.vn>
2020-03-17 21:40:52 +0700

    temp

---
+++ 04_View/ThinhN/BaiTap1/BaiTap1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BaiTap1
+//
+//  Created by ADMIN on 3/17/2563 BE.
+//  Copyright © 2563 BE asiantech. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
