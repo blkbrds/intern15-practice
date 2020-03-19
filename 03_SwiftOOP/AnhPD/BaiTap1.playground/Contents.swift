05e64ab5a07495416241abf24047d08a397b595c
Anh Pham D.[2] <anh.pham2@asiantech.vn>
2020-03-19 17:31:58 +0700

    Done bài tập 1 OOP

--- 03_SwiftOOP/AnhPD/BaiTap1.playground/Contents.swift
+++ 03_SwiftOOP/AnhPD/BaiTap1.playground/Contents.swift
@@ -1,48 +1,53 @@
 import UIKit
 
 class PhanSo {
-    var tuso: Double
-    var mauso: Double
+    var tuSo: Int
+    var mauSo: Int
     
-    init(tuso: Double,mauso: Double) {
-        self.tuso = tuso
-        self.mauso = mauso
+    init(tuSo: Int,mauSo: Int) {
+        self.tuSo = tuSo
+        self.mauSo = mauSo
     }
-    func inPhanSo() -> String {
-        return "\(tuso) / \(mauso)"
+    
+    func inPhanSo() {
+        print("\(tuSo) / \(mauSo)")
     }
-    func Cong(phanso: PhanSo) -> PhanSo {
-        let tu = tuso * phanso.mauso + phanso.tuso * mauso
-        let mau = mauso * phanso.mauso
-        return PhanSo (tuso: tu,mauso: mau)
+    
+    func phepCong(phanSo: PhanSo) -> String {
+        var tu = tuSo * phanSo.mauSo + phanSo.tuSo * mauSo
+        var mau = mauSo * phanSo.mauSo
+        return("\(tu) / \(mau)")
     }
-    func Tru(phanso: PhanSo) -> PhanSo {
-        let tu = tuso + phanso.mauso - phanso.tuso * mauso
-        let mau = mauso * phanso.mauso
-        return PhanSo(tuso: tu, mauso: mau)
+    func phepTru(phanSo: PhanSo) -> String {
+        var tu = tuSo * phanSo.mauSo - phanSo.tuSo * mauSo
+        var mau = mauSo * phanSo.mauSo
+        return("\(tu) / \(mau)")
     }
-    func Nhan(phanso: PhanSo) -> PhanSo {
-        let tu = tuso * phanso.tuso
-        let mau = mauso * phanso.mauso
-        return PhanSo(tuso: tu, mauso: mau)
+    func phepNhan(phanSo: PhanSo) -> String {
+        var tu = tuSo * phanSo.tuSo
+        var mau = mauSo * phanSo.mauSo
+        return("\(tu) / \(mau)")
     }
-    func Chia(phanso: PhanSo) -> PhanSo {
-        let tu = tuso * phanso.mauso
-        let mau = phanso.tuso * mauso
-        return PhanSo(tuso: tu, mauso: mau)
+    func phepChia(phanSo: PhanSo) -> String  {
+        var tu = tuSo * phanSo.mauSo
+        var mau = phanSo.tuSo * mauSo
+        return("\(tu) / \(mau)")
     }
 }
-var ps1 = PhanSo(tuso: 2, mauso:4)
-var ps2 = PhanSo(tuso: 1, mauso: 6)
 
-var cong = ps1.Cong(phanso: ps2)
-var tru = ps1.Tru(phanso: ps2)
-var nhan = ps1.Nhan(phanso: ps2)
-var chia = ps1.Chia(phanso: ps2)
+var pS1 = PhanSo(tuSo: 3, mauSo: 2)
+var pS2 = PhanSo(tuSo: 2, mauSo: 6)
 
-print(" Ps1 + Ps2 = \(cong.inPhanSo())")
-print(" Ps1 + Ps2 = \(tru.inPhanSo())")
-print(" Ps1 + Ps2 = \(nhan.inPhanSo())")
-print(" Ps1 + Ps2 = \(chia.inPhanSo())")
+
+var Cong = pS1.phepCong(phanSo: pS2)
+var Tru = pS1.phepTru(phanSo: pS2)
+var Nhan = pS1.phepNhan(phanSo: pS2)
+var Chia = pS1.phepChia(phanSo: pS2)
+print(pS1.inPhanSo())
+print(Cong)
+print(Tru)
+print(Nhan)
+print(Chia)
+
 
 
