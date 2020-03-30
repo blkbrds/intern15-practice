6bc4f4f6c09f2b5c847e5827b3ad17b5280fc8aa
Anh Pham D.[2] <anh.pham2@asiantech.vn>
2020-03-30 12:20:11 +0700

    Fix BaiTap1 - Phan CustomView

--- 05_CustomView/AnhPD/BaiTap1/BaiTap1/ViewController.swift
+++ 05_CustomView/AnhPD/BaiTap1/BaiTap1/ViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  BaiTap1
-//
-//  Created by PCI0012 on 3/30/20.
-//  Copyright © 2020 PCI0012. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController, MyAvatarDelegate {
@@ -37,16 +29,4 @@ class ViewController: UIViewController, MyAvatarDelegate {
     func didTap(name: String) {
         print("\(name)")
     }
-
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
