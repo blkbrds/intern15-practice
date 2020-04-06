e719a654667d5ce26212d87caa78ace76027ce4b
Lam Le <lam.le2@asiantech.vn>
2020-04-06 08:52:52 +0700

    no message

---
+++ 11_TableView/LamLT/Bai5/Bai5/HomeViewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var nameSearchBar: UISearchBar!
+    @IBOutlet weak var nameTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+}
