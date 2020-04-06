f57bbd4a0c25209097ae8416f1468f7f6e41d1a6
Lam Le <lam.le2@asiantech.vn>
2020-04-06 16:20:47 +0700

    Up bài 9

--- 11_TableView/LamLT/Bai9/Bai9/HomeCotroller/HomeViewController.swift
+++ 11_TableView/LamLT/Bai9/Bai9/HomeCotroller/HomeViewController.swift
@@ -2,56 +2,55 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
-    @IBOutlet weak var tableview: UITableView!
+    @IBOutlet weak var tableView: UITableView!
     var contacts: [[String]] = [[]]
-    var titles: [String] = ["Đông Vật", "Thức Ăn"]
+    var titles: [String] = ["ANIMAL","FOOD"]
     var sectionTitles: [String] {
         return titles.compactMap { $0.first?.uppercased() }
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Home"
         loadData()
         configTableView()
     }
+    
     func loadData() {
-        guard let path = Bundle.main.url(forResource: "DataList", withExtension: "plist")
+        guard let path = Bundle.main.url(forResource: "NameList", withExtension: "plist")
             else { return }
         guard let contactsData = NSArray(contentsOf: path) as? [[String]]
             else { return }
         contacts = contactsData
     }
-
+    
     func configTableView() {
-        tableview.register(UITableViewCell.self, forCellReuseIdentifier: "UITableViewCell")
-        tableview.dataSource = self
+        let nib = UINib(nibName: "CustomTableViewCell", bundle: Bundle.main)
+        tableView.register(nib, forCellReuseIdentifier: "CustomTableViewCell")
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 }
 
-extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+extension HomeViewController: UITableViewDataSource, UITableViewDelegate {
     func numberOfSections(in tableView: UITableView) -> Int {
         return contacts.count
     }
-
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return contacts[section].count
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
-        cell.textLabel?.text = contacts[indexPath.section][indexPath.row]
-        let nameLabel = UILabel(frame: CGRect(x: 40, y: 25, width:  199, height: 30))
-        nameLabel.text = "Name"
-        nameLabel.textColor = .red
-        cell.addSubview(nameLabel)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CustomTableViewCell", for: indexPath) as! CustomTableViewCell
+        cell.updateTableCell(avatar: "jao", contactName: contacts[indexPath.section][indexPath.row], subTitle: "Click")
         return cell
     }
-
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("selected cell: \(contacts[indexPath.section][indexPath.row])")
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
     }
-
+    
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return titles[section]
     }
@@ -61,3 +60,11 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     }
 }
 
+extension HomeViewController: CustomTableViewCellDelegate {
+    func customTableView(customTableView: CustomTableViewCell, needPerformAction action: CustomTableViewCell.Action) {
+        switch action {
+        case .sendTitle(let title):
+            print("Click \(title)")
+        }
+    }
+}
