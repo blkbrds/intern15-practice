933d69c8159bc5ad53df458b7db8a03b4e0830d1
Lam Le <lam.le2@asiantech.vn>
2020-04-06 17:03:57 +0700

    no message

---
+++ 11_TableView/LamLT/Bai10/Bai10/HomeController/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  Bai10
+//
+//  Created by PCI0002 on 4/6/20.
+//  Copyright © 2020 LeThanhLam. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ 11_TableView/LamLT/Bai11/Bai11/CustomCell/ElasticityTableViewCell.swift
@@ -0,0 +1,15 @@
+
+
+import UIKit
+
+class ElasticityTableViewCell: UITableViewCell {
+
+    @IBOutlet private weak var messageLabel: UILabel!
+
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+
+}
