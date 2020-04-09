a0359b902e1a411bf03d5545c32b84a1d1006c0b
QuanBuiThien <quan.bui@asiantech.vn>
2020-04-09 09:30:18 +0700

    Commit fix code

--- 07_Autolayout/QuanBT/BaiTapAutoLayout/BaiTapAutoLayout/SceneDelegate.swift
+++ 07_Autolayout/QuanBT/BaiTapAutoLayout/BaiTapAutoLayout/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = AutoLayoutViewController()
+        window.rootViewController = AutoLayout5_5ViewController()
         self.window = window
         window.makeKeyAndVisible()
         
