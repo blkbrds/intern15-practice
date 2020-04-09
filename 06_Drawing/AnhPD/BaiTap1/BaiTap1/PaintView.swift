36ec821f5aeb22a1f515ddd0afb1d186e89c4b17
Anh Pham D.[2] <anh.pham2@asiantech.vn>
2020-04-09 14:46:52 +0700

    Commit Fix code Bai Tap1 Phan Drawing

--- 06_Drawing/AnhPD/BaiTap1/BaiTap1/PaintView.swift
+++ 06_Drawing/AnhPD/BaiTap1/BaiTap1/PaintView.swift
@@ -2,24 +2,44 @@ import UIKit
 
 class PaintView: UIView {
 
-    func drawHinhChuNhat(rect: CGRect,color: UIColor) -> UIView {
-        let view1 = UIView(frame: rect)
-        view1.backgroundColor = color
-        return view1
-    }
-    
-    func draw(start: CGPoint,end: CGPoint) {
-        let bezierPath = UIBezierPath()
-        bezierPath.move(to: start)
-        bezierPath.addLine(to: end)
-        bezierPath.close()
+    func draw()  {
+        var toaDoY: Int = 650
+        let wayPath: Int = 4
+        for _ in 0...wayPath {
+            let bezierPath = UIBezierPath()
+            bezierPath.move(to: CGPoint(x: 80, y: toaDoY))
+            bezierPath.addLine(to: CGPoint(x: 350, y: toaDoY))
+            bezierPath.close()
+            
+            let shapeLayer = CAShapeLayer()
+            shapeLayer.strokeColor = UIColor.black.cgColor
+            shapeLayer.lineWidth = 1
+            shapeLayer.path = bezierPath.cgPath
+            
+            self.layer.addSublayer(shapeLayer)
+            
+            toaDoY -= 80
+        }
         
-        let shapeLayer = CAShapeLayer()
-        shapeLayer.strokeColor = UIColor.black.cgColor
-        shapeLayer.lineWidth = 1
-        shapeLayer.path = bezierPath.cgPath
+        let rectangleView = UIView(frame: CGRect(x: 100, y: 450, width: 20, height: 200))
+        rectangleView.backgroundColor = .red
+        self.addSubview(rectangleView)
         
-        self.layer.addSublayer(shapeLayer)
-    }
+        let rectangle1View = UIView(frame: CGRect(x: 150, y: 470, width: 20, height: 180))
+        rectangle1View.backgroundColor = .blue
+        self.addSubview(rectangle1View)
+        
+        let rectangle2View = UIView(frame: CGRect(x: 200, y: 400, width: 20, height: 250))
+        rectangle2View.backgroundColor = .green
+        self.addSubview(rectangle2View)
+        
+        let rectangle3View = UIView(frame: CGRect(x: 250, y: 490, width: 20, height: 160))
+        rectangle3View.backgroundColor = .black
+        self.addSubview(rectangle3View)
+        
+        let rectangle4View = UIView(frame: CGRect(x: 300, y: 420, width: 20, height: 230))
+        rectangle4View.backgroundColor = .gray
+        self.addSubview(rectangle4View)
 
+    }
 }
--- 06_Drawing/AnhPD/BaiTap1/BaiTap1/ViewController.swift
+++ 06_Drawing/AnhPD/BaiTap1/BaiTap1/ViewController.swift
@@ -5,30 +5,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        var toaDoY: Int = 650
-        
-        for i in 0...3 {
-            let wayPath = PaintView()
-            wayPath.draw(start: CGPoint(x: 80, y: toaDoY), end: CGPoint(x: 350, y: toaDoY))
-            toaDoY -= 80
-            view.addSubview(wayPath)
-        }
-        
-        let rectangle = PaintView()
-        let rectangleView = rectangle.drawHinhChuNhat(rect: CGRect(x: 100, y: 450, width: 20, height: 200), color: .red)
-        view.addSubview(rectangleView)
-        
-        let rectangle1View = rectangle.drawHinhChuNhat(rect: CGRect(x: 150, y: 400, width: 20, height: 250), color: .black)
-        view.addSubview(rectangle1View)
-        
-        let rectangle2View = rectangle.drawHinhChuNhat(rect: CGRect(x: 200, y: 470, width: 20, height: 180), color: .gray)
-        view.addSubview(rectangle2View)
-        
-        let rectangle3View = rectangle.drawHinhChuNhat(rect: CGRect(x: 250, y: 490, width: 20, height: 160), color: .green)
-        view.addSubview(rectangle3View)
-        
-        let rectangle4View = rectangle.drawHinhChuNhat(rect: CGRect(x: 300, y: 420, width: 20, height: 230), color: .blue)
-        view.addSubview(rectangle4View)
-        
+        let paint = PaintView()
+        paint.draw()
+        view.addSubview(paint)
     }
 }
