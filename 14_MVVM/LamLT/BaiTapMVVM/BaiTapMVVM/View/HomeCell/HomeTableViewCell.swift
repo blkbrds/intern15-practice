9c11460c52749d5995c4bb86e047caba6bd28f36
Lam Le <lam.le2@asiantech.vn>
2020-04-16 14:31:28 +0700

    Up bài 1

---
+++ 14_MVVM/LamLT/BaiTapMVVM/BaiTapMVVM/View/HomeCell/HomeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HomeTableViewCell.swift
+//  BaiTapMVVM
+//
+//  Created by PCI0002 on 4/16/20.
+//  Copyright © 2020 LeThanhLam. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
