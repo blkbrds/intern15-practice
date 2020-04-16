00fd4fb0c5d60ce3640ea996669caa641535113a
Lam Le <lam.le2@asiantech.vn>
2020-04-16 16:22:49 +0700

    Update layouy

--- 14_MVVM/LamLT/BaiTapMVVM/BaiTapMVVM/Model/DataManager.swift
+++ 14_MVVM/LamLT/BaiTapMVVM/BaiTapMVVM/Model/DataManager.swift
@@ -26,17 +26,3 @@ class DataManager {
         self.favorite = favorite
     }
 }
-
-final class DataDetail {
-    var name: String
-    var image: String
-    var comment: String
-    var time: String
-
-    init(name: String = "", image: String = "", comment: String = "", time: String = "") {
-        self.name = name
-        self.image = image
-        self.comment = comment
-        self.time = time
-    }
-}
