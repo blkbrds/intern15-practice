3327d2f3b10a4a89025e8354cfe85477ec6a65c4
Thinh Nguyen P .[6] <thinh.nguyen6@asiantech.vn>
2020-04-20 13:35:02 +0700

    bai tap 8 wip

---
+++ 08_MVC/ThinhN/baiTapMVC/baiTapMVC/baiTap1/CalculatorViewController.swift
@@ -0,0 +1,41 @@
+//
+//  CalculatorViewController.swift
+//  baiTapMVC
+//
+//  Created by ADMIN on 4/16/2563 BE.
+//  Copyright © 2563 BE asiantech. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    @IBOutlet weak var showResult: UILabel!
+    var numberScreen: Float = 0
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+   
+    @IBAction func chooseNumberTouchUpInsine(_ sender: UIButton) {
+        
+    
+    }
+    
+    @IBAction func chooseOperatorButtonTouchUpInsine(_ sender: UIButton) {
+        
+        
+    }
+    @IBAction func clearButtonTouchUpInsine(_ sender: UIButton) {
+    }
+    
+    @IBAction func resultButtonTouchUpInsine(_ sender: UIButton) {
+    }
+    
+    
+}
---
+++ 08_MVC/ThinhN/baiTapMVC/baiTapMVC/tinhtoan/tinhToan.swift
@@ -0,0 +1,19 @@
+//
+//  tinhToan.swift
+//  baiTapMVC
+//
+//  Created by ADMIN on 4/17/2563 BE.
+//  Copyright © 2563 BE asiantech. All rights reserved.
+//
+
+import Foundation
+enum phepTinh: String{
+    case cong = "+"
+    case tru = "-"
+    case nhan = "x"
+    case chia = "/"
+}
+final class tinhToan{
+    let share: tinhToan = tinhToan()
+    
+}
