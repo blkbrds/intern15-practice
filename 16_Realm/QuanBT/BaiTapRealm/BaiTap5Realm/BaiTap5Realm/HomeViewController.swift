6388c4e9d7f44778f6457d677f8ea58c62979741
QuanBuiThien <quan.bui@asiantech.vn>
2020-04-22 13:00:39 +0700

    Commit bài tập 5

---
+++ 16_Realm/QuanBT/BaiTapRealm/BaiTap5Realm/BaiTap5Realm/HomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  HomeViewController.swift
+//  BaiTap5Realm
+//
+//  Created by Sếp Quân on 4/22/20.
+//  Copyright © 2020 QuanBT. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
