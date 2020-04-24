4507e95d0babcc8742cd45844fdae44c1a0bcd9d
QuanBuiThien <quan.bui@asiantech.vn>
2020-04-24 09:59:09 +0700

    Commit bài tập áp dụng

---
+++ 16_Realm/QuanBT/BaiTapRealm/BaiTapAPIMVVM/BaiTapAPIMVVM/Views/HomeTableViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  HomeTableViewModel.swift
+//  BaiTapAPIMVVM
+//
+//  Created by Sếp Quân on 4/14/20.
+//  Copyright © 2020 QuanBT. All rights reserved.
+//
+
+import Foundation
+
+final class HomeTableViewModel {
+    // MARK: - Properties
+    var dataAPI: DataAPI = DataAPI()
+    
+    // MARK: - Init
+    init(dataAPI: DataAPI = DataAPI()) {
+        self.dataAPI = dataAPI
+    }
+}
