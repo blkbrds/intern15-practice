e19f96ab4373f7c3fee9e9be2d55d753ae64d55a
Thinh Nguyen P .[6] <thinh.nguyen6@asiantech.vn>
2020-04-29 10:17:51 +0700

    lam bai test

---
+++ BaiTapLayout/ThinhN/Baitest/Baitest/AutoLayoutViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AutoLayoutViewController.swift
+//  Baitest
+//
+//  Created by ADMIN on 4/29/2563 BE.
+//  Copyright © 2563 BE asiantech. All rights reserved.
+//
+
+import UIKit
+
+class AutoLayoutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
