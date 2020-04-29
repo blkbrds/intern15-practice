c5589a01130c21aae24999379c06be61f54be764
Lam Le <lam.le2@asiantech.vn>
2020-04-29 11:14:04 +0700

    Update Layout

--- BaiTapLayout/LamLT/Bai2/Bai2/Model/DataPerson.swift
+++ BaiTapLayout/LamLT/Bai2/Bai2/Model/DataPerson.swift
@@ -5,5 +5,6 @@ final class DataPerson {
     // MARK: - Properties
     var namePerson: [String] = ["Nguyễn Văn A","Trần Thị Bé"]
     var address: [String] = ["Hải Châu Đà Nẵng","Hải Châu Đà Nẵng Hải Châu Đà Nẵng Hải Châu Đà Nẵng"]
-    var inforPerson: [String] = ["",""]
+    var inforPerson: [String] = ["20/01/2000","15/09/2004"]
+    var sexPerson: [String] = ["Nam","Nữ"]
 }
