fd92bf69612e47a2f2679455ff10e1e229437b0a
Anh Pham D.[2] <anh.pham2@asiantech.vn>
2020-04-29 10:51:14 +0700

    Commit Bai tap test layout

---
+++ BaiTapLayout/AnhPD/BaiTapTestLayout/BaiTapTestLayout/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  BaiTapTestLayout
+//
+//  Created by PCI0012 on 4/29/20.
+//  Copyright © 2020 PCI0012. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
