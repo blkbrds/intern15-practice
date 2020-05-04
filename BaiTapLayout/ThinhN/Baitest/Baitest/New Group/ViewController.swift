38550365a670e5eb820ed23d799eeaf4cf182348
Thinh Nguyen P .[6] <thinh.nguyen6@asiantech.vn>
2020-05-04 11:41:59 +0700

    fix lỗi

---
+++ BaiTapLayout/ThinhN/Baitest/Baitest/New Group/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Baitest
+//
+//  Created by ADMIN on 4/29/2563 BE.
+//  Copyright © 2563 BE asiantech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+
+}
