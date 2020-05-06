c6daf96ffc6b0b7a78a6ce4c5d1b5b8159f7c065
thinhnguyen6 <thinh.nguyen6@asiantech.vn>
2020-05-06 15:46:19 +0700

    Em nop bai test

---
+++ BaiTapLayout/ThinhN/Baitest/BaiTest2/BaiTest2/ViewAvatar/DetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DetailViewController.swift
+//  BaiTest2
+//
+//  Created by PCI0019 on 5/6/20.
+//  Copyright © 2020 asiantech. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Detail"
+        
+    }
+
+}
---
+++ BaiTapLayout/ThinhN/Baitest/BaiTest2/BaiTest2/ViewAvatar/HomeViewController.swift
@@ -0,0 +1,42 @@
+//
+//  HomeViewController.swift
+//  BaiTest2
+//
+//  Created by PCI0019 on 5/6/20.
+//  Copyright © 2020 asiantech. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var onlineButton: UIButton!
+    @IBOutlet weak var offButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Home"
+        onlineButton.layer.masksToBounds = true
+        onlineButton.layer.cornerRadius = 15
+        onlineButton.clipsToBounds = true
+        onlineButton.layer.borderWidth = 2
+        
+        offButton.layer.masksToBounds = true
+        offButton.layer.cornerRadius = 15
+        offButton.clipsToBounds = true
+        offButton.layer.borderWidth = 2
+    }
+    
+    @IBAction func onlineButtonTouchUpInsine(_ sender: Any) {
+        let vc = DetailViewController()
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @IBAction func OffButtonTouchUpInsine(_ sender: Any) {
+        let vc = DetailViewController()
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    
+    
+}
