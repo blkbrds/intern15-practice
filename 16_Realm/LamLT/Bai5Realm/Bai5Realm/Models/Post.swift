da1a57862e11983002f7a3723e9e761026c60b39
Lam Le <lam.le2@asiantech.vn>
2020-05-06 14:07:48 +0700

    Update Bai Realm

--- 16_Realm/LamLT/Bai5Realm/Bai5Realm/Models/Post.swift
+++ 16_Realm/LamLT/Bai5Realm/Bai5Realm/Models/Post.swift
@@ -5,5 +5,5 @@ final class Post: Object {
     @objc dynamic var title = ""
     @objc dynamic var content = ""
     @objc dynamic var createDate = ""
-    @objc dynamic var isFavorite = false
+    @objc dynamic var isFavorite = true
 }
