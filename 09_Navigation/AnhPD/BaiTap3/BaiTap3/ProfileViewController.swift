60fae77e0b3e2aea38c895790b4844073c7a4ed4
Anh Pham D.[2] <anh.pham2@asiantech.vn>
2020-05-07 11:42:29 +0700

    Update bai tap 3

---
+++ 09_Navigation/AnhPD/BaiTap3/BaiTap3/ProfileViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ProfileViewController.swift
+//  BaiTap3
+//
+//  Created by PCI0012 on 5/6/20.
+//  Copyright © 2020 PCI0012. All rights reserved.
+//
+
+import UIKit
+
+protocol ProfileViewControllerDelegate {
+    func updateData(userName: String, index: Int)
+}
+
+class ProfileViewController: UIViewController {
+
+    @IBOutlet weak var avatarProfileImageView: UIImageView!
+    @IBOutlet weak var userNameProfileTextField: UITextField!
+    
+    var userName: String = ""
+    var imageView: String = ""
+    var index: Int?
+    var delegate: ProfileViewControllerDelegate?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Profile"
+        loadName()
+        configNavigationBar()
+        // Do any additional setup after loading the view.
+    }
+    
+    func configNavigationBar() {
+        let backButton = UIBarButtonItem(title: "<Back", style: .plain, target: self, action: #selector(backTouchUpInSide))
+        navigationItem.leftBarButtonItem = backButton
+        
+        let doneButton = UIBarButtonItem(title: "Done>", style: .plain, target: self, action: #selector(doneButtonTouchUpInSide))
+        navigationItem.rightBarButtonItem = doneButton
+    }
+    
+    @objc func backTouchUpInSide() {
+        if let navigationController = navigationController {
+            navigationController.popViewController(animated: true)
+        }
+    }
+    
+    @objc func doneButtonTouchUpInSide() {
+        if let delegate = delegate, let index = index {
+            delegate.updateData(userName: userNameProfileTextField.text ?? "", index: index)
+        }
+        
+        if let navigationController = navigationController {
+            navigationController.popViewController(animated: true)
+        }
+    }
+    
+    private func loadName() {
+        userNameProfileTextField.text = userName
+        avatarProfileImageView.image = UIImage(named: imageView)
+    }
+}
