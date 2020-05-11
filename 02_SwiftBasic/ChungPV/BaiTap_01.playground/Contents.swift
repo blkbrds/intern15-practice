ce681c03fcdfa0f6321fe699368b78786b63e3d1
Chung <chung.pham@asiantech.vn>
2020-05-11 19:56:15 +0900

    Finish 1.Gioi thieu, 2.Basic Swift

---
+++ 02_SwiftBasic/ChungPV/BaiTap_01.playground/Contents.swift
@@ -0,0 +1,10 @@
+import Foundation
+public func dientich_thetich(r : Float ) -> (s : Float, v: Float) {
+    var s: Float = 0
+    var v: Float = 0
+    
+    s = 4*3.14*pow(r,2)
+    v = (4/3)*3.14*pow(r, 3)
+    return (s,v)
+}
+dientich_thetich(r: 5)
