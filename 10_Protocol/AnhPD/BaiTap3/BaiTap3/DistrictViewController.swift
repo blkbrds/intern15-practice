dbd8fda6ab432ed4e92a6d713b3257d91a476eac
Anh Pham D.[2] <anh.pham2@asiantech.vn>
2020-05-12 14:21:44 +0700

    Fix comment bai tap 3

--- 10_Protocol/AnhPD/BaiTap3/BaiTap3/DistrictViewController.swift
+++ 10_Protocol/AnhPD/BaiTap3/BaiTap3/DistrictViewController.swift
@@ -49,31 +49,31 @@ class DistrictViewController: UIViewController {
     @IBAction func DistrictButtonTouchUpInSide(_ sender: UIButton) {
         switch sender.tag {
         case 0:
-            setupButton(i: 0)
+            chooseButtonTouchUpInSide(i: 0)
         case 1:
-            setupButton(i: 1)
+            chooseButtonTouchUpInSide(i: 1)
         case 2:
-            setupButton(i: 2)
+            chooseButtonTouchUpInSide(i: 2)
         case 3:
-            setupButton(i: 3)
+            chooseButtonTouchUpInSide(i: 3)
         case 4:
-            setupButton(i: 4)
+            chooseButtonTouchUpInSide(i: 4)
         case 5:
-            setupButton(i: 5)
+            chooseButtonTouchUpInSide(i: 5)
         case 6:
-            setupButton(i: 6)
+            chooseButtonTouchUpInSide(i: 6)
         case 7:
-            setupButton(i: 7)
+            chooseButtonTouchUpInSide(i: 7)
         case 8:
-            setupButton(i: 8)
+            chooseButtonTouchUpInSide(i: 8)
         case 9:
-            setupButton(i: 9)
+            chooseButtonTouchUpInSide(i: 9)
         default:
             print("Error")
         }
     }
     
-    func setupButton(i: Int) {
+    func  chooseButtonTouchUpInSide(i: Int) {
         for index in 0..<districtButtons.count {
             districtButtons[index].backgroundColor = .clear
             districtButtons[index].setTitleColor(.black, for: .normal)
