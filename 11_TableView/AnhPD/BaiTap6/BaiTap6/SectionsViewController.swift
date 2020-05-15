097f31ea55f549cda933935d10ef60081b5b133f
Anh Pham D.[2] <anh.pham2@asiantech.vn>
2020-05-15 14:51:59 +0700

    Hoan thanh bai tap 7

---
+++ 11_TableView/AnhPD/BaiTap6/BaiTap6/SectionsViewController.swift
@@ -0,0 +1,84 @@
+//
+//  SectionsViewController.swift
+//  BaiTap6
+//
+//  Created by PCI0012 on 5/15/20.
+//  Copyright © 2020 PCI0012. All rights reserved.
+//
+
+import UIKit
+
+class SectionsViewController: UIViewController {
+
+    @IBOutlet weak var sectionsTableView: UITableView!
+    
+    var cattles = [String]()
+    var items = [String]()
+	var foods = [String]()
+
+	var titlesSection: [String] = ["Cattles", "Items","Foods"]
+	var sections: [[String]] = []
+	var sectionIndex: [String] = ["C","I","F"]
+	
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "SECTIONS"
+        updateData()
+		sectionsTableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+		sectionsTableView.dataSource = self
+		sectionsTableView.delegate = self
+    }
+    
+    func updateData() {
+        guard let path = Bundle.main.url(forResource: "Cattle", withExtension: "plist") else {
+            return
+        }
+        guard let cattlesData = NSArray(contentsOf: path) as? [String] else {
+            return
+        }
+        guard let path1 = Bundle.main.url(forResource: "Items", withExtension: "plist") else {
+            return
+        }
+        guard let itemsData = NSArray(contentsOf: path1) as? [String] else {
+            return
+        }
+		guard let path2 = Bundle.main.url(forResource: "Foods", withExtension: "plist") else {
+			return
+		}
+		guard let foosData = NSArray(contentsOf: path2) as? [String] else {
+			return
+		}
+		
+		cattles = cattlesData
+		items = itemsData
+		foods = foosData
+		sections = [cattles, items, foods]
+    }
+}
+
+extension SectionsViewController: UITableViewDelegate, UITableViewDataSource {
+	func numberOfSections(in tableView: UITableView) -> Int {
+		return titlesSection.count
+	}
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return sections[section].count
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = sectionsTableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+		cell.textLabel?.text = sections[indexPath.section][indexPath.row]
+		return cell
+	}
+	
+	func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+		return titlesSection[section]
+	}
+	
+	func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+		return sectionIndex
+	}
+	
+	func tableView(_ tableView: UITableView, sectionForSectionIndexTitle title: String, at index: Int) -> Int {
+		return index
+	}
+}
