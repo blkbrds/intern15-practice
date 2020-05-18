5bd6ebde16036abb8f604676a290341ccba41c5c
thinhnguyen6 <thinh.nguyen6@asiantech.vn>
2020-05-18 17:43:41 +0700

    Hoàn thành bài 1, 2 OOP

--- 03_SwiftOOP/ThinhN(2)/MyPlayground.playground/Contents.swift
+++ 03_SwiftOOP/ThinhN(2)/MyPlayground.playground/Contents.swift
@@ -1,51 +1,113 @@
 import UIKit
 
 //Tạo lớp Người với các thuộc tính cơ bản như tên, ngày sinh, tuổi (tuổi được tính từ ngày sinh và ngày hiện tại), phương thức là hoạt động. Yêu cầu tạo 1 đối tượng thuộc lớp Người có tên là “Nguyễn Văn A”, ngày sinh là 1/1/1988. In ra tuổi của đối tương này
-class Nguoi {
+//class Nguoi {
+//    var ten: String
+//    var ngaySinh: String
+//    var tuoi: Int {
+//        get {
+//            let ngay = ngaySinh.split(separator: "/")
+//            print(ngay)
+//            let nam = Int(String(ngay[2])) ?? 0
+//            return 2020 - nam
+//        }
+//    }
+//    init (ten: String, ngaySinh: String) {
+//        self.ten = ten
+//        self.ngaySinh = ngaySinh
+//    }
+//    func hoatDong() {
+//        print("Hoat dong ")
+//    }
+//}
+//class GiaoVien: Nguoi {
+//    var boMon: String
+//    init(ten: String, ngaySinh: String, boMon: String) {
+//        self.boMon = boMon
+//        super.init(ten: ten, ngaySinh: ngaySinh)
+//    }
+//    override func hoatDong() {
+//        print("Giang day")
+//    }
+//}
+//
+//class HocSinh: Nguoi {
+//    var lop: String
+//    init(ten: String, ngaySinh: String, lop: String) {
+//        self.lop = lop
+//        super.init(ten: ten, ngaySinh: ngaySinh)
+//    }
+//    override func hoatDong() {
+//        print("Hoc tap ")
+//    }
+//}
+//var a = Nguoi(ten: "X", ngaySinh: "20/02/2002")
+//a.hoatDong()
+
+//class SoPhuc {
+//    var phanThuc: Int
+//    var phanAo: Int
+//    init(phanThuc: Int, phanAo: Int) {
+//        self.phanThuc = phanThuc
+//        self.phanAo = phanAo
+//    }
+//    func hienSoPhuc() {
+//        print("\(phanThuc) + \(phanAo)i")
+//    }
+//    func congSoPhuc(a: Int, b: Int) {
+//        let giaTriA = phanThuc + a
+//        let giaTriB = phanAo + b
+//        print("\(giaTriA) + \(giaTriB)i")
+//    }
+//    func nhanSoPhuc(a: Int, b: Int) {
+//        let giaTriA = phanThuc * a  - b * phanAo
+//        let giaTriB = (phanThuc * b + a * phanAo)
+//        print("\(giaTriA) + \(giaTriB)i")
+//    }
+//}
+//var soNhap = SoPhuc(phanThuc: 3, phanAo: 2)
+//soNhap.hienSoPhuc()
+//soNhap.congSoPhuc(a: 4, b: -2)
+//soNhap.nhanSoPhuc(a: 1, b: -1)
+
+class ThiSinh {
+    var ma: Int
     var ten: String
-    var ngaySinh: String
-    var tuoi: Int {
-        get {
-            let ngay = ngaySinh.split(separator: "/")
-            let nam = Int(String(ngay[2])) ?? 0
-            return 2020 - nam
+    var ngayThangNamSinh: String
+    var diemToan: Int
+    var diemVan: Int
+    var diemAnh: Int
+    init(ma: Int, ten: String, ngayThangNamSinh: String, diemToan: Int, diemVan: Int, diemAnh: Int){
+        self.ma = ma
+        self.ten = ten
+        self.ngayThangNamSinh = ngayThangNamSinh
+        self.diemToan = diemToan
+        self.diemAnh = diemAnh
+        self.diemVan = diemVan
+    }
+    func tongDiem() -> Int {
+        let tongSoDiem = diemToan + diemVan + diemAnh
+        return tongSoDiem
+    }
+    func inThongTin() {
+        print("\(ma) \(ten) \(ngayThangNamSinh) \(diemToan) \(diemVan) \(diemAnh)")
+    }
+}
+class KiemTraThiSinh {
+    var cacThiSinh: [ThiSinh]
+    init(cacThiSinh: [ThiSinh]) {
+        self.cacThiSinh = cacThiSinh
+    }
+    func kiemTra() {
+        for i in cacThiSinh {
+            if i.tongDiem() > 15 {
+                i.inThongTin()
+            }
         }
     }
-    init (ten: String, ngaySinh: String) {
-        self.ten = ten
-        self.ngaySinh = ngaySinh
-    }
-    
-    func hoatDong() {
-        print("Hoat dong ")
-    }
 }
-
-class GiaoVien: Nguoi {
-    var boMon: String
-    init(ten: String, ngaySinh: String, boMon: String) {
-        self.boMon = boMon
-        super.init(ten: ten, ngaySinh: ngaySinh)
-    }
-    override func hoatDong() {
-        print("Giang day")
-    }
-}
-
-class HocSinh: Nguoi {
-    var lop: String
-    init(ten: String, ngaySinh: String, lop: String) {
-        self.lop = lop
-        super.init(ten: ten, ngaySinh: ngaySinh)
-    }
-    override func hoatDong() {
-        print("Hoc tap ")
-    }
-}
-var a = Nguoi(ten: "X", ngaySinh: "20/02/2002")
-a.hoatDong()
-a.tuoi
-var b = GiaoVien(ten: "x2", ngaySinh: "20/02/2222", boMon: "Toan")
-b.hoatDong()
-
-
+var a = ThiSinh(ma: 2, ten: "ff", ngayThangNamSinh:
+    "22/4/24", diemToan: 5, diemVan: 6, diemAnh: 8)
+var b = ThiSinh(ma: 3, ten: "faf", ngayThangNamSinh: "14/4/22", diemToan: 4, diemVan: 6, diemAnh: 7)
+var danhSach = KiemTraThiSinh(cacThiSinh: [ThiSinh(ma: 1, ten: "LeVanA", ngayThangNamSinh: "22/2/2002", diemToan: 5, diemVan: 4, diemAnh: 5), ThiSinh(ma: 2, ten: "TranBa", ngayThangNamSinh: "22/5/2014", diemToan: 8, diemVan: 8, diemAnh: 6)])
+danhSach.kiemTra()
