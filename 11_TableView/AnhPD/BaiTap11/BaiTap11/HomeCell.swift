5fa1997ea98083e388d6b62ce471a6d7a5e61d1c
Anh Pham D.[2] <anh.pham2@asiantech.vn>
2020-05-21 14:36:23 +0700

    Hoan thanh bai tap 11

---
+++ 11_TableView/AnhPD/BaiTap11/BaiTap11/HomeCell.swift
@@ -0,0 +1,19 @@
+//
+//  HomeCell.swift
+//  BaiTap11
+//
+//  Created by PCI0012 on 5/21/20.
+//  Copyright © 2020 PCI0012. All rights reserved.
+//
+
+import UIKit
+
+class HomeCell: UITableViewCell {
+
+    @IBOutlet weak var textLable: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+}
