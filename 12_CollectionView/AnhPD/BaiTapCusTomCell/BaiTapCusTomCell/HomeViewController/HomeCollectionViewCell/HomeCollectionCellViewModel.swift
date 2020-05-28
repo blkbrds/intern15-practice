87c4f414b624f80dd2ef0a3f62988c8d18932ca9
Anh Pham D.[2] <anh.pham2@asiantech.vn>
2020-05-28 17:34:15 +0700

    Bai tap lam them Custom Collection Cell

---
+++ 12_CollectionView/AnhPD/BaiTapCusTomCell/BaiTapCusTomCell/HomeViewController/HomeCollectionViewCell/HomeCollectionCellViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  HomeCollectionCellViewModel.swift
+//  BaiTapCusTomCell
+//
+//  Created by PCI0012 on 5/28/20.
+//  Copyright © 2020 PCI0012. All rights reserved.
+//
+
+import Foundation
---
+++ 12_CollectionView/AnhPD/BaiTapCusTomCell/BaiTapCusTomCell/HomeViewController/HomeCollectionViewCell/HomeCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  HomeCollectionViewCell.swift
+//  BaiTapCusTomCell
+//
+//  Created by PCI0012 on 5/28/20.
+//  Copyright © 2020 PCI0012. All rights reserved.
+//
+
+import UIKit
+
+class HomeCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
