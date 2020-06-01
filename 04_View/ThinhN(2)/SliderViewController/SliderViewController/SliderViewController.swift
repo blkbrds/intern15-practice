7bde2aabb65a6fe230beeee07c7b4d3f8581c33e
thinhnguyen6 <thinh.nguyen6@asiantech.vn>
2020-06-01 13:33:02 +0700

    done excercise 5

--- 04_View/ThinhN(2)/SliderViewController/SliderViewController/SliderViewController.swift
+++ 04_View/ThinhN(2)/SliderViewController/SliderViewController/SliderViewController.swift
@@ -9,32 +9,59 @@
 import UIKit
 
 class SliderViewController: UIViewController {
-
+    var thumbnailView: UIView?
+    var blueSliderView: UIView?
+    var thumbnailLabel: UILabel?
+    var whiteSliderView: UIView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-    let whiteSliderView = UIView(frame: CGRect(x: 150, y: 150, width: 20, height: 500))
-        whiteSliderView.backgroundColor = .white
-        whiteSliderView.layer.borderWidth = 1
-        view.addSubview(whiteSliderView)
-    let blueSliderView = UIView(frame: CGRect(x: 150, y: 400, width: 20, height: 250))
-        blueSliderView.backgroundColor = .systemBlue
-        view.addSubview(blueSliderView)
-    let thumbnailView = UIView(frame: CGRect(x: 140, y: 390, width: 40, height: 40))
-        thumbnailView.backgroundColor = .orange
-        thumbnailView.layer.cornerRadius = 20
-        let thumbnailLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
-        thumbnailLabel.text = "50"
-        thumbnailLabel.textAlignment = .center
-        thumbnailLabel.textColor = .white
-        thumbnailLabel.font = .systemFont(ofSize: 10)
-        thumbnailView.addSubview(thumbnailLabel)
-        view.addSubview(thumbnailView)
-        let slideGesture = UIGestureRecognizer(target: self, action: #selector(self.sliderGesture(_:)))
-        thumbnailLabel.addGestureRecognizer(slideGesture)
-        
+        whiteSliderView = UIView(frame: CGRect(x: 150, y: 150, width: 20, height: 500))
+        if let whiteSliderView = whiteSliderView {
+            whiteSliderView.backgroundColor = .white
+            whiteSliderView.layer.borderWidth = 1
+            view.addSubview(whiteSliderView)
+        }
+        blueSliderView = UIView(frame: CGRect(x: 150, y: 400, width: 20, height: 250))
+        if let blueSliderView = blueSliderView {
+            blueSliderView.backgroundColor = .systemBlue
+            view.addSubview(blueSliderView)
+        }
+        thumbnailView = UIView(frame: CGRect(x: 140, y: 390, width: 40, height: 40))
+        thumbnailLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
+        if let thumbnailLabel = thumbnailLabel {
+            thumbnailLabel.text = "50"
+            thumbnailLabel.textAlignment = .center
+            thumbnailLabel.textColor = .white
+            thumbnailLabel.font = .systemFont(ofSize: 10)
+        }
+        if let thumbnailView = thumbnailView {
+            thumbnailView.backgroundColor = .orange
+            thumbnailView.layer.cornerRadius = 20
+            if let thumbnailLabel = thumbnailLabel {
+                thumbnailView.addSubview(thumbnailLabel)
+                view.addSubview(thumbnailView)
+            }
+        }
     }
-    @objc func sliderGesture(_ sender: UITapGestureRecognizer) {
-        print("aaa")
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let position = touch.location(in: self.view)
+            let textLabel: Int =  Int((650 - position.y) / 500 * 100)
+            if textLabel <= 100 && textLabel > 0 {
+                thumbnailLabel?.text = String(textLabel)
+                if position.y < 150 && position.y > 650 {
+                    return
+                }
+                if touch.view == thumbnailView {
+                    if var frame = thumbnailView?.frame {
+                    frame.origin.y = position.y
+                    thumbnailView?.frame = frame
+                    blueSliderView?.frame.origin = CGPoint(x: 150, y: position.y)
+                    blueSliderView?.frame.size = CGSize(width: 20, height: 650 - position.y)
+                    }
+                }
+            }
+        }
     }
-            
 }
