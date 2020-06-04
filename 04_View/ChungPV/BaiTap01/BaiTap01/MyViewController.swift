424c3bb2f12cd6d2007f33fde6886ec0e1641719
Chung <chung.pham@asiantech.vn>
2020-06-05 00:44:13 +0900

    Change convention name Baitap01

--- 04_View/ChungPV/BaiTap01/BaiTap01/MyViewController.swift
+++ 04_View/ChungPV/BaiTap01/BaiTap01/MyViewController.swift
@@ -10,28 +10,30 @@ import UIKit
 
 class MyViewController: UIViewController {
 
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Add user avatar
+        
+        //avatar
         let frame = CGRect(x: 50, y: 100, width: 100, height: 100)
-        let userAvatar = UIImageView(image: UIImage(named: "avatar.png"))
-        userAvatar.frame = frame
-        userAvatar.contentMode = .scaleToFill
-        view.addSubview(userAvatar)
+        let avatarImageView = UIImageView(image: UIImage(named: "avatar.png"))
+        avatarImageView.frame = frame
+        avatarImageView.contentMode = .scaleToFill
+        view.addSubview(avatarImageView)
         
         // Add user name
-        let userName = UILabel(frame: CGRect(x: 50, y: 200, width: 100, height: 50))
-        userName.text = "Name"
-        userName.textAlignment = .center
-        userName.backgroundColor = .blue
-        userName.textColor = .white
-        view.addSubview(userName)
+        let nameLabel = UILabel(frame: CGRect(x: 50, y: 200, width: 100, height: 30))
+        nameLabel.text = "Name"
+        nameLabel.textAlignment = .center
+        nameLabel.backgroundColor = .blue
+        nameLabel.textColor = .white
+        view.addSubview(nameLabel)
         
         // Add button
-        let button = UIButton(frame: CGRect(x: 50, y: 100, width: 100, height: 250))
-        button.backgroundColor = .clear
-        button.addTarget(self, action: #selector(buttonDidclick), for: .touchUpInside)
-        view.addSubview(button)
+        let nameButton = UIButton(frame: CGRect(x: 50, y: 100, width: 100, height: 130))
+        nameButton.backgroundColor = .clear
+        nameButton.addTarget(self, action: #selector(buttonDidclick), for: .touchUpInside)
+        view.addSubview(nameButton)
         
         }
         @objc func buttonDidclick() {
