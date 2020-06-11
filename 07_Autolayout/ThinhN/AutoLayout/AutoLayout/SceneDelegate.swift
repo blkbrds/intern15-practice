372933fc23ad14c8ed0fe200696a90cd05e4d056
thinhnguyen6 <thinh.nguyen6@asiantech.vn>
2020-06-11 12:04:53 +0700

    Fix comment Autolayout

--- 07_Autolayout/ThinhN/AutoLayout/AutoLayout/SceneDelegate.swift
+++ 07_Autolayout/ThinhN/AutoLayout/AutoLayout/SceneDelegate.swift
@@ -10,15 +10,15 @@ import UIKit
 import SwiftUI
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return}
-            let window = UIWindow(windowScene: windowScene)
-        let viewController = BaiTap14ViewController()
+        let window = UIWindow(windowScene: windowScene)
+        let viewController = Bai9ViewController()
         self.window = window
         window.rootViewController = viewController
-            window.makeKeyAndVisible()
-        }
+        window.makeKeyAndVisible()
     }
+}
