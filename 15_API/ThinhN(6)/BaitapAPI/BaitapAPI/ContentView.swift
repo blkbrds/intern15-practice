52a04914fc31f9fe09e3d55f8174f3875c7f6168
Thinh Nguyen P .[6] <thinh.nguyen6@asiantech.vn>
2020-08-12 13:55:58 +0700

    Commit bài tập 1,2 API

---
+++ 15_API/ThinhN(6)/BaitapAPI/BaitapAPI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BaitapAPI
+//
+//  Created by ADMIN on 8/11/2563 BE.
+//  Copyright © 2563 BE asiantech. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
